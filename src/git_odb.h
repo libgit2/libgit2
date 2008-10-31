bce499af70d434e89c9a110374f1b7c6dd3621df
Shawn O. Pearce <spearce@spearce.org>
2008-10-31 11:01:28 -0700

    Add a GIT_ prefix to OBJ_ constants to scope them better
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- src/git_odb.h
+++ src/git_odb.h
@@ -71,15 +71,15 @@ GIT_EXTERN(void) git_odb_close(git_odb** db);
 
 /** Basic type (loose or packed) of any Git object. */
 typedef enum {
-	OBJ_BAD = -1,       /**< Object is invalid. */
-	OBJ__EXT1 = 0,      /**< Reserved for future use. */
-	OBJ_COMMIT = 1,     /**< A commit object. */
-	OBJ_TREE = 2,       /**< A tree (directory listing) object. */
-	OBJ_BLOB = 3,       /**< A file revision object. */
-	OBJ_TAG = 4,        /**< An annotated tag object. */
-	OBJ__EXT2 = 5,      /**< Reserved for future use. */
-	OBJ_OFS_DELTA = 6,  /**< A delta, base is given by an offset. */
-	OBJ_REF_DELTA = 7,  /**< A delta, base is given by object id. */
+	GIT_OBJ_BAD = -1,       /**< Object is invalid. */
+	GIT_OBJ__EXT1 = 0,      /**< Reserved for future use. */
+	GIT_OBJ_COMMIT = 1,     /**< A commit object. */
+	GIT_OBJ_TREE = 2,       /**< A tree (directory listing) object. */
+	GIT_OBJ_BLOB = 3,       /**< A file revision object. */
+	GIT_OBJ_TAG = 4,        /**< An annotated tag object. */
+	GIT_OBJ__EXT2 = 5,      /**< Reserved for future use. */
+	GIT_OBJ_OFS_DELTA = 6,  /**< A delta, base is given by an offset. */
+	GIT_OBJ_REF_DELTA = 7,  /**< A delta, base is given by object id. */
 } git_otype;
 
 /** A small object read from the database. */
