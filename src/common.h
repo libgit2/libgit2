76a8c44727406a76d251102bdee607871887a119
Andreas Ericsson <ae@op5.se>
2008-11-22 14:42:12 +0100

    Add internal common.h file
    This one pulls in compiler compatibility macros, some
    common header files, and also the public common.h header.
    
    C source files are modified to use the private common.h
    in favour of the public one.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

---
+++ src/common.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_common_h__
+#define INCLUDE_common_h__
+
+#include "cc-compat.h"
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "git/common.h"
+#endif /* INCLUDE_common_h__ */
