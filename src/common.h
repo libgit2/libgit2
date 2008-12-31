064301cc3528ec3bccd07dafe9af824d87a50dba
Shawn O. Pearce <spearce@spearce.org>
2008-12-30 22:07:56 -0800

    Fix size_t snprintf warning by using PRIuPTR format macro
    This is the correct C99 format code for the size_t type when passed
    as an argument to the *printf family.  If the platform doesn't
    define it, we assume %lu and just cross our fingers that its the
    proper setting for a size_t on this system.  On most sane platforms,
    "unsigned long" is the underlying type of "size_t".
    
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- src/common.h
+++ src/common.h
@@ -8,11 +8,16 @@
 #ifdef GIT_HAS_PTHREAD
 # include <pthread.h>
 #endif
+#include <inttypes.h>
 #include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+#ifndef PRIuPTR
+# define PRIuPTR "lu"
+#endif
 
 #include "git/common.h"
 
