25e9b4dd449d58af5edefe33d383292d6ead5003
Vicent Marti <tanoku@gmail.com>
2010-11-24 17:48:31 +0200

    Do proper symbol exports on MSVC
    Yes, if you are wondering why the shared library was
    failing to build under MSVC, it's because it was empty.
    
    Oh wow.
    
    Signed-off-by: Vicent Marti <tanoku@gmail.com>

--- src/git/common.h
+++ src/git/common.h
@@ -19,6 +19,8 @@
 # define GIT_EXTERN(type) extern \
 			  __attribute__((visibility("default"))) \
 			  type
+#elif defined(_MSC_VER)
+# define GIT_EXTERN(type) __declspec(dllexport) type
 #else
 # define GIT_EXTERN(type) extern type
 #endif
