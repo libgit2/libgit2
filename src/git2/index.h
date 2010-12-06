f591833095ad9a3bbff42c76531131f7ec6a2954
Vicent Marti <tanoku@gmail.com>
2010-12-06 23:08:31 +0200

    Add license file to all public headers
    Yes, the public headers do need our license.
    
    Signed-off-by: Vicent Marti <tanoku@gmail.com>

--- src/git2/index.h
+++ src/git2/index.h
@@ -1,3 +1,27 @@
+/*
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2,
+ * as published by the Free Software Foundation.
+ *
+ * In addition to the permissions in the GNU General Public License,
+ * the authors give you unlimited permission to link the compiled
+ * version of this file into combinations with other programs,
+ * and to distribute those combinations without any restriction
+ * coming from the use of this file.  (The General Public License
+ * restrictions do apply in other respects; for example, they cover
+ * modification of the file, and distribution when not linked into
+ * a combined executable.)
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 #ifndef INCLUDE_git_index_h__
 #define INCLUDE_git_index_h__
 
@@ -5,7 +29,7 @@
 #include "types.h"
 
 /**
- * @file git/index.h
+ * @file git2/index.h
  * @brief Git index parsing and manipulation routines
  * @defgroup git_index Git index parsing and manipulation routines
  * @ingroup Git
