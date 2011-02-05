1af8c7488d99e4eeeec31c5a1e7e43cd003983e3
nulltoken <emeric.fermas@gmail.com>
2011-02-05 15:24:08 +0100

    Enforced refs handling tests.
     - Added a test to ensure that a nested symbolic reference is properly resolved.
     - Added comparisons of object ids.

--- tests/t10-refs.c
+++ tests/t10-refs.c
@@ -60,6 +60,7 @@ BEGIN_TEST("readtag", non_existing_tag_reference_looking_up)
 END_TEST
 
 static const char *head_ref_name = "HEAD";
+static const char *head_tracker_sym_ref_name = "head-tracker";
 static const char *current_head_target = "refs/heads/master";
 static const char *current_master_tip = "be3563ae3f795b2b4353bcce3a527ad0a4f7f644";
 
@@ -89,24 +90,64 @@ BEGIN_TEST("readsymref", symbolic_reference_looking_up)
 	git_repository_free(repo);
 END_TEST
 
-BEGIN_TEST("readsymref", looking_up_head_then_master)
+BEGIN_TEST("readsymref", nested_symbolic_reference_looking_up)
 	git_repository *repo;
-	git_reference *reference;
+	git_reference *reference, *resolved_ref;
+	git_object *object;
+	git_oid id;
 
 	must_pass(git_repository_open(&repo, REPOSITORY_FOLDER));
+
+	must_pass(git_repository_lookup_ref(&reference, repo, head_tracker_sym_ref_name));
+	must_be_true(reference->type == GIT_REF_SYMBOLIC);
+	must_be_true(reference->packed == 0);
+	must_be_true(strcmp(reference->name, head_tracker_sym_ref_name) == 0);
+
+	must_pass(git_reference_resolve(&resolved_ref, reference));
+	must_be_true(resolved_ref->type == GIT_REF_OID);
+
+	must_pass(git_repository_lookup(&object, repo, git_reference_oid(resolved_ref), GIT_OBJ_ANY));
+	must_be_true(object != NULL);
+	must_be_true(git_object_type(object) == GIT_OBJ_COMMIT);
+
+	git_oid_mkstr(&id, current_master_tip);
+	must_be_true(git_oid_cmp(&id, git_object_id(object)) == 0);
+
+	git_repository_free(repo);
+END_TEST
+
+BEGIN_TEST("readsymref", looking_up_head_then_master)
+	git_repository *repo;
+	git_reference *reference, *resolved_ref, *comp_base_ref;
+
+	must_pass(git_repository_open(&repo, REPOSITORY_FOLDER));
+
+	must_pass(git_repository_lookup_ref(&reference, repo, head_tracker_sym_ref_name));
+	must_pass(git_reference_resolve(&resolved_ref, reference));
+	comp_base_ref = resolved_ref;
+
 	must_pass(git_repository_lookup_ref(&reference, repo, head_ref_name));
+	must_pass(git_reference_resolve(&resolved_ref, reference));
+	must_pass(git_oid_cmp(git_reference_oid(comp_base_ref), git_reference_oid(resolved_ref)));
+
 	must_pass(git_repository_lookup_ref(&reference, repo, current_head_target));
+	must_pass(git_reference_resolve(&resolved_ref, reference));
+	must_pass(git_oid_cmp(git_reference_oid(comp_base_ref), git_reference_oid(resolved_ref)));
 
 	git_repository_free(repo);
 END_TEST
 
 BEGIN_TEST("readsymref", looking_up_master_then_head)
 	git_repository *repo;
-	git_reference *reference, *master_ref;
+	git_reference *reference, *master_ref, *resolved_ref;
 
 	must_pass(git_repository_open(&repo, REPOSITORY_FOLDER));
+
 	must_pass(git_repository_lookup_ref(&master_ref, repo, current_head_target));
 	must_pass(git_repository_lookup_ref(&reference, repo, head_ref_name));
+
+	must_pass(git_reference_resolve(&resolved_ref, reference));
+	must_pass(git_oid_cmp(git_reference_oid(master_ref), git_reference_oid(resolved_ref)));
 
 	git_repository_free(repo);
 END_TEST
@@ -154,6 +195,7 @@ git_testsuite *libgit2_suite_refs(void)
 	ADD_TEST(suite, "readtag", loose_tag_reference_looking_up);
 	ADD_TEST(suite, "readtag", non_existing_tag_reference_looking_up);
 	ADD_TEST(suite, "readsymref", symbolic_reference_looking_up);
+	ADD_TEST(suite, "readsymref", nested_symbolic_reference_looking_up);
 	ADD_TEST(suite, "readsymref", looking_up_head_then_master);
 	ADD_TEST(suite, "readsymref", looking_up_master_then_head);
 	ADD_TEST(suite, "readpackedref", packed_reference_looking_up);
