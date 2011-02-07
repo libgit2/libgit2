becff0428e7df20792bdfc143d33c0d03b448385
Vicent Marti <tanoku@gmail.com>
2011-02-07 08:09:11 +0200

    Fix compilation in MSVC
    The git_odb_backend_* symbols were being redefined as external.
    
    Signed-off-by: Vicent Marti <tanoku@gmail.com>

--- src/odb.h
+++ src/odb.h
@@ -14,8 +14,4 @@ struct git_odb {
 int git_odb__hash_obj(git_oid *id, char *hdr, size_t n, int *len, git_rawobj *obj);
 int git_odb__inflate_buffer(void *in, size_t inlen, void *out, size_t outlen);
 
-
-int git_odb_backend_loose(git_odb_backend **backend_out, const char *objects_dir);
-int git_odb_backend_pack(git_odb_backend **backend_out, const char *objects_dir);
-
 #endif
