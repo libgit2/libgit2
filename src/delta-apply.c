e416774e2afc820e5ecf288c6bdc7a019db67072
Jakob Pfender <jpfender@elegosoft.com>
2011-05-17 17:22:33 +0200

    delta-apply.c: Move to new error handling mechanism

--- src/delta-apply.c
+++ src/delta-apply.c
@@ -46,10 +46,10 @@ int git__delta_apply(
 	 * base object, resulting in data corruption or segfault.
 	 */
 	if ((hdr_sz(&base_sz, &delta, delta_end) < 0) || (base_sz != base_len))
-		return GIT_ERROR;
+		return git__throw(GIT_ERROR, "Failed to apply delta. Base size does not match given data");
 
 	if (hdr_sz(&res_sz, &delta, delta_end) < 0)
-		return GIT_ERROR;
+		return git__throw(GIT_ERROR, "Failed to apply delta. Base size does not match given data");
 
 	if ((res_dp = git__malloc(res_sz + 1)) == NULL)
 		return GIT_ENOMEM;
@@ -105,5 +105,5 @@ int git__delta_apply(
 fail:
 	free(out->data);
 	out->data = NULL;
-	return GIT_ERROR;
+	return git__throw(GIT_ERROR, "Failed to apply delta");
 }
