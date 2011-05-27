890888b4030de158865149316f95bb3c40c34ae8
Marc Pegon <pegon.marc@gmail.com>
2011-05-27 22:37:10 +0200

    Added error for ambiguous oid prefixes. Added methods to compare the first nth hexadecimal characters (i.e. packets of 4 bits) of OIDs.

---
+++ src/oid.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_oid_h__
+#define INCLUDE_oid_h__
+
+/* This can be useful for internal use */
+int git_oid_match_raw(unsigned int len, const unsigned char *a, const unsigned char *b);
+
+#endif
