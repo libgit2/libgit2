e5c7a9fe5480818da098fc0a9fd26fe966a953f4
Jakob Pfender <jpfender@elegosoft.com>
2011-06-23 12:32:15 +0200

    index.c: Return ENOTFOUND when trying to init nonexistant path
    When calling git_index_add or git_index_append on a path that does not
    exist, the correct return code thrown by index_init_entry should be
    GIT_ENOTFOUND instead of GIT_EOSERR.
    
    This fixes a problem originally fixed by fdd1e04 through the
    introduction of gitfo_shallow_exists, which was removed by
    ae49695.

--- src/index.c
+++ src/index.c
@@ -412,7 +412,7 @@ static int index_init_entry(git_index_entry *entry, git_index *index, const char
 	git__joinpath(full_path, index->repository->path_workdir, rel_path);
 
 	if (gitfo_lstat(full_path, &st) < 0)
-		return git__throw(GIT_EOSERR, "Failed to initialize entry. '%s' cannot be opened", full_path);
+		return git__throw(GIT_ENOTFOUND, "Failed to initialize entry. '%s' cannot be opened", full_path);
 
 	if (stage < 0 || stage > 3)
 		return git__throw(GIT_ERROR, "Failed to initialize entry. Invalid stage %i", stage);
