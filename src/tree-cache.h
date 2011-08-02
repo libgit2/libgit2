3ba69ba8a469deee81ab718ecc04551c17b9615d
Carlos Martín Nieto <cmn@elego.de>
2011-08-02 15:27:42 +0200

    Add git_tree_cache_get
    Signed-off-by: Carlos Martín Nieto <cmn@elego.de>

--- src/tree-cache.h
+++ src/tree-cache.h
@@ -25,6 +25,7 @@ typedef struct git_tree_cache git_tree_cache;
 
 int git_tree_cache_read(git_tree_cache **tree, const char *buffer, size_t buffer_size);
 void git_tree_cache_invalidate_path(git_tree_cache *tree, const char *path);
+const git_tree_cache *git_tree_cache_get(const git_tree_cache *tree, const char *path);
 void git_tree_cache_free(git_tree_cache *tree);
 
 #endif
