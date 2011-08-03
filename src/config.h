e737136ab04b7022fa6357bb12bede0a516bbe1c
Carlos Martín Nieto <carlos@cmartin.tk>
2011-08-03 22:03:57 +0200

    Bind the configuration and remotes to a repository
    Configurations when taken from a repository and remotes should be
    identifiable as coming from a particular repository. This allows us to
    reduce the amount of variables that the user has to keep track of.
    
    Signed-off-by: Carlos Martín Nieto <carlos@cmartin.tk>

--- src/config.h
+++ src/config.h
@@ -4,12 +4,14 @@
 #include "git2.h"
 #include "git2/config.h"
 #include "vector.h"
+#include "repository.h"
 
 #define GIT_CONFIG_FILENAME ".gitconfig"
 #define GIT_CONFIG_FILENAME_INREPO "config"
 
 struct git_config {
 	git_vector files;
+	git_repository *repo;
 };
 
 #endif
