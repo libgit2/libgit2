48c33796a1548f81530f51bc7089eeaa4c69a65b
Carlos Martín Nieto <carlos@cmartin.tk>
2011-08-19 09:03:19 +0200

    Fix Windows compilation
    Sockets on Windows are unsigned, so define a type GIT_SOCKET which is
    signed or unsigned depending on the platform.
    
    Thanks to Em for his patience with this.
    
    Signed-off-by: Carlos Martín Nieto <carlos@cmartin.tk>

--- src/netops.h
+++ src/netops.h
@@ -4,11 +4,17 @@
 #ifndef INCLUDE_netops_h__
 #define INCLUDE_netops_h__
 
+#ifndef _WIN32
+typedef int GIT_SOCKET;
+#else
+typedef unisgned int GIT_SOCKET;
+#endif
+
 typedef struct gitno_buffer {
 	char *data;
 	unsigned int len;
 	unsigned int offset;
-	int fd;
+	GIT_SOCKET fd;
 } gitno_buffer;
 
 void gitno_buffer_setup(gitno_buffer *buf, char *data, unsigned int len, int fd);
@@ -18,5 +24,6 @@ void gitno_consume_n(gitno_buffer *buf, unsigned int cons);
 
 int gitno_connect(const char *host, const char *port);
 int gitno_send(int s, const char *msg, int len, int flags);
+int gitno_select_in(gitno_buffer *buf, long int sec, long int usec);
 
 #endif
