f9d4b0c39569148c1fe5a4e48c00f26325f2875f
Carlos Martín Nieto <carlos@cmartin.tk>
2011-09-12 17:25:46 +0200

    git_repository_config: open global config file automatically
    If the global configuration file is missing, it is ignored.
    
    Signed-off-by: Carlos Martín Nieto <carlos@cmartin.tk>

--- src/repository.c
+++ src/repository.c
@@ -288,7 +288,6 @@ cleanup:
 int git_repository_config(
 		git_config **out,
 		git_repository *repo,
-		const char *user_config_path,
 		const char *system_config_path)
 {
 	char config_path[GIT_PATH_MAX];
@@ -305,10 +304,9 @@ int git_repository_config(
 	if (error < GIT_SUCCESS)
 		goto cleanup;
 
-	if (user_config_path != NULL) {
-		error = git_config_add_file_ondisk(*out, user_config_path, 2);
-		if (error < GIT_SUCCESS)
-			goto cleanup;
+	error = git_config_find_global(config_path);
+	if (error == GIT_SUCCESS) {
+		error = git_config_add_file_ondisk(*out, config_path, 2);
 	}
 
 	if (system_config_path != NULL) {
