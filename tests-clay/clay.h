d02a7d833083518353ede8c940410d5229e83e48
Vicent Marti <tanoku@gmail.com>
2011-09-28 18:57:32 +0200

    Update Clay

--- tests-clay/clay.h
+++ tests-clay/clay.h
@@ -48,11 +48,46 @@ void cl_fixture_cleanup(const char *fixture_name);
 #define cl_check_fail(expr) cl_check_fail_((expr), NULL)
 #define cl_check(expr) cl_check_((expr), NULL)
 
-
 /**
  * Forced failure/warning
  */
 #define cl_fail(desc) clay__assert(0, __FILE__, __LINE__, "Test failed.", desc, 1)
 #define cl_warning(desc) clay__assert(0, __FILE__, __LINE__, "Warning during test execution:", desc, 0)
+
+/**
+ * Test method declarations
+ */
+extern void test_core_dirent__dont_traverse_dot(void);
+extern void test_core_dirent__traverse_subfolder(void);
+extern void test_core_dirent__traverse_slash_terminated_folder(void);
+extern void test_core_dirent__dont_traverse_empty_folders(void);
+extern void test_core_dirent__traverse_weird_filenames(void);
+extern void test_core_filebuf__0(void);
+extern void test_core_filebuf__1(void);
+extern void test_core_filebuf__2(void);
+extern void test_core_path__0(void);
+extern void test_core_path__1(void);
+extern void test_core_path__2(void);
+extern void test_core_path__5(void);
+extern void test_core_path__6(void);
+extern void test_core_rmdir__initialize(void);
+extern void test_core_rmdir__delete_recursive(void);
+extern void test_core_rmdir__fail_to_delete_non_empty_dir(void);
+extern void test_core_string__0(void);
+extern void test_core_string__1(void);
+extern void test_core_vector__0(void);
+extern void test_core_vector__1(void);
+extern void test_core_vector__2(void);
+extern void test_network_remotes__initialize(void);
+extern void test_network_remotes__cleanup(void);
+extern void test_network_remotes__parsing(void);
+extern void test_network_remotes__refspec_parsing(void);
+extern void test_network_remotes__fnmatch(void);
+extern void test_network_remotes__transform(void);
+extern void test_status_single__hash_single_file(void);
+extern void test_status_worktree__initialize(void);
+extern void test_status_worktree__cleanup(void);
+extern void test_status_worktree__whole_repository(void);
+extern void test_status_worktree__empty_repository(void);
 
 #endif
