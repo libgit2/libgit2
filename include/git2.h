3eaf34f4c602b9e155e2f4c6ae26c9250ac37d50
Vicent Marti <tanoku@gmail.com>
2011-10-05 16:48:36 -0400

    libgit2 v0.15.0 "Das Wunderbar Release"
    I am aware the codename is not gramatically correct in any language.
    
    Check the COPYING file for the detailed terms on libgit2's license. Check
    the AUTHORS file for the full list of guilty parties.
    
    As we slowly stabilize the API, we've dropped 1 function from the library,
    and changed the signature of only 5 of them. There's of course a good
    chunk of new functionality, and a thousand bug fixes.
    
    In this release of libgit2:
    
    	- Changed `git_blob_rawsize`: Now returns `size_t` instead of int, allowing
    	files >4GB in 64 bit systems.
    
    	- Removed `git_commit_message_short`: Please use `git_commit_message`
    	to get the full message and decide which is the "short view" according
    	to your needs (first line, first 80 chars...)
    
    	- Added `git_commit_message_encoding`: Returns the encoding field of a commit
    	message, if it exists.
    
    	- Changed `git_commit_create`, `git_commit_create_v`: New argument `encoding`, which
    	adds a encoding field to the generated commit object.
    
    	- Added `git_config_find_system`: Returns the path to the system's global config
    	file (according to the Core Git standards).
    
    	- Changed `git_config_get_XX`, `git_config_set_XX`: the `long` and `int` types have
    	been replaced by `int64` and `int32` respectively, to make their meaning more
    	obvious.
    
    	- Added `git_indexer`: An interface to index Git Packfiles has been added in the
    	`git2/indexer.h` header.
    
    	- Changed `git_reflog_entry_XX`: Reflog entries are now returned as `git_oid *` objects
    	instead of hexadecimal OIDs.
    
    	- Added `git_remote`: More fetch functionality has been added to the `git2/remote.h`
    	functionality. Local, Smart HTTP and Git protocols are now supported.
    
    	- Added `git_repository_head`: Returns the HEAD of the repository.
    
    	- Added `git_repository_config_autoload`: Opens the configuration file of a repository,
    	including the user's and the system's global config files, if they can be found.
    
    	- Changed `git_signature_now`: Now returns an error code; the signature is stored by
    	reference.

--- include/git2.h
+++ include/git2.h
@@ -8,9 +8,9 @@
 #ifndef INCLUDE_git_git_h__
 #define INCLUDE_git_git_h__
 
-#define LIBGIT2_VERSION "0.14.0"
+#define LIBGIT2_VERSION "0.15.0"
 #define LIBGIT2_VER_MAJOR 0
-#define LIBGIT2_VER_MINOR 14
+#define LIBGIT2_VER_MINOR 15
 #define LIBGIT2_VER_REVISION 0
 
 #include "git2/common.h"
