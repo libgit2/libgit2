96fab093e3d7fa15e5085f36c51841fa6628cbe9
Sven Strickroth <email@cs-ware.de>
2011-10-09 18:37:41 +0200

    put version information in separate file
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

---
+++ include/git2/version.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2011 the libgit2 contributors
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
+#ifndef INCLUDE_git_version_h__
+#define INCLUDE_git_version_h__
+
+#define LIBGIT2_VERSION "0.15.0"
+#define LIBGIT2_VER_MAJOR 0
+#define LIBGIT2_VER_MINOR 15
+#define LIBGIT2_VER_REVISION 0
+
+#endif
