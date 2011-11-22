1d09a1c88ddde32bf9b6b5ab6a9feab2ecdaa6ff
Vicent Marti <tanoku@gmail.com>
2011-11-22 01:41:22 +0100

    clay: Merge manually @leto's tests from #485
    This uses the new Clay code. As you can see, the diff is minimal... It
    works!

--- tests-clay/clay.h
+++ tests-clay/clay.h
@@ -57,6 +57,8 @@ void cl_fixture_cleanup(const char *fixture_name);
 /**
  * Test method declarations
  */
+extern void test_buf_basic__printf(void);
+extern void test_buf_basic__resize(void);
 extern void test_config_stress__cleanup(void);
 extern void test_config_stress__dont_break_on_invalid_input(void);
 extern void test_config_stress__initialize(void);
