99abb79d53781dda9bbdc7b896eaa383d8789cb3
nulltoken <emeric.fermas@gmail.com>
2012-02-03 12:45:43 +0100

    repository: ensure that the path to the .git directory ends with a forward slash when opening a repository through a working directory path
    This fixes an issue which was detected while using one of the libgit2 bindings [0]. The lack of the trailing forward slash led the name of references returned by git_reference_listall() to be prefixed with a forward slash.
    
      [0]: https://github.com/libgit2/libgit2sharp/pull/108

--- tests-clar/repo/open.c
+++ tests-clar/repo/open.c
@@ -1,24 +1,46 @@
 #include "clar_libgit2.h"
 #include "posix.h"
 
-void test_repo_open__bare_empty_repo(void)
+static git_repository *repo;
+
+void test_repo_open__cleanup(void)
 {
-	git_repository *repo;
-
-	cl_git_pass(git_repository_open(&repo, cl_fixture("empty_bare.git")));
-	cl_assert(git_repository_path(repo) != NULL);
-	cl_assert(git_repository_workdir(repo) == NULL);
-
 	git_repository_free(repo);
 }
 
-void test_repo_open__standard_empty_repo(void)
+void test_repo_open__bare_empty_repo(void)
 {
-	git_repository *repo;
+	cl_git_pass(git_repository_open(&repo, cl_fixture("empty_bare.git")));
 
-	cl_git_pass(git_repository_open(&repo, cl_fixture("empty_standard_repo/.gitted")));
 	cl_assert(git_repository_path(repo) != NULL);
-	cl_assert(git_repository_workdir(repo) != NULL);
+	cl_assert(git__suffixcmp(git_repository_path(repo), "/") == 0);
 
-	git_repository_free(repo);
+	cl_assert(git_repository_workdir(repo) == NULL);
+}
+
+void test_repo_open__standard_empty_repo_through_gitdir(void)
+{
+	cl_git_pass(git_repository_open(&repo, cl_fixture("empty_standard_repo/.gitted")));
+
+	cl_assert(git_repository_path(repo) != NULL);
+	cl_assert(git__suffixcmp(git_repository_path(repo), "/") == 0);
+
+	cl_assert(git_repository_workdir(repo) != NULL);
+	cl_assert(git__suffixcmp(git_repository_workdir(repo), "/") == 0);
+}
+
+void test_repo_open__standard_empty_repo_through_workdir(void)
+{
+	cl_fixture_sandbox("empty_standard_repo");
+	cl_git_pass(p_rename("empty_standard_repo/.gitted", "empty_standard_repo/.git"));
+
+	cl_git_pass(git_repository_open(&repo, "empty_standard_repo"));
+
+	cl_assert(git_repository_path(repo) != NULL);
+	cl_assert(git__suffixcmp(git_repository_path(repo), "/") == 0);
+
+	cl_assert(git_repository_workdir(repo) != NULL);
+	cl_assert(git__suffixcmp(git_repository_workdir(repo), "/") == 0);
+
+	cl_fixture_cleanup("empty_standard_repo");
 }
