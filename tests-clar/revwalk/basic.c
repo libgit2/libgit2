abab0a94be6147ab48f71273b3dc83243ed94a0a
Carlos Martín Nieto <carlos@cmartin.tk>
2012-02-28 02:19:57 +0100

    revwalk: allow pushing/hiding a reference by name
    The code was already there, so factor it out and let users push an OID
    by giving it a reference name. Only refs to commits are
    supported. Annotated tags will throw an error.

--- tests-clar/revwalk/basic.c
+++ tests-clar/revwalk/basic.c
@@ -148,14 +148,13 @@ void test_revwalk_basic__push_head(void)
 	cl_assert(i == 7);
 }
 
-void test_revwalk_basic__push_head_hide_glob(void)
+void test_revwalk_basic__push_head_hide_ref(void)
 {
 	int i = 0;
 	git_oid oid;
 
 	cl_git_pass(git_revwalk_push_head(_walk));
-	/* This is a hack, as we know this will only match the packed-test branch */
-	cl_git_pass(git_revwalk_hide_glob(_walk, "heads/packed-test*"));
+	cl_git_pass(git_revwalk_hide_ref(_walk, "refs/heads/packed-test"));
 
 	while (git_revwalk_next(&oid, _walk) == GIT_SUCCESS) {
 		i++;
