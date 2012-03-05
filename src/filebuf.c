a461d615484e4361b4a86850ae9184e4e36d6bb6
Ryan Wilcox <rwilcox@wilcoxd.com>
2012-03-05 12:10:02 -0500

    forgot else statement

--- src/filebuf.c
+++ src/filebuf.c
@@ -299,8 +299,8 @@ cleanup:
 	{
 		if (error == GIT_ERROR)
 			git__rethrow(error, "Failed to commit locked file from buffer (%s)", strerror(errno));
-
-		return git__rethrow(error, "Failed to commit locked file from buffer");
+		else
+			return git__rethrow(error, "Failed to commit locked file from buffer");
 	}
 	return GIT_SUCCESS;
 }
