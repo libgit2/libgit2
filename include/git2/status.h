95340398a1821bd19da1bfe459ba1f375ed89404
Russell Belfer <arrbee@arrbee.com>
2012-03-22 09:17:34 -0700

    Adding new tests for new status command
    This is a work in progress.  This adds two new sets of tests,
    the issue_592 tests from @nulltoken's pull request #601 and
    some new tests for submodules.  The submodule tests still have
    issues where the status is not reported correctly.  That needs
    to be fixed before merge.

--- include/git2/status.h
+++ include/git2/status.h
@@ -31,8 +31,7 @@ GIT_BEGIN_DECL
 #define GIT_STATUS_WT_MODIFIED		(1 << 4)
 #define GIT_STATUS_WT_DELETED		(1 << 5)
 
-#define GIT_STATUS_IGNORED			(1 << 6)
-#define GIT_STATUS_WT_UNTRACKED		(1 << 7)
+#define GIT_STATUS_IGNORED		(1 << 6)
 
 /**
  * Gather file statuses and run a callback for each one.
