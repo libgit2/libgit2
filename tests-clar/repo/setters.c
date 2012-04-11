1de77cd31432a1bdff060181c6d9ec25a412a0c2
Russell Belfer <rb@github.com>
2012-04-11 12:10:14 -0700

    Cannot set workdir to a nonexistent dir

--- tests-clar/repo/setters.c
+++ tests-clar/repo/setters.c
@@ -1,5 +1,6 @@
 #include "clar_libgit2.h"
 #include "buffer.h"
+#include "posix.h"
 
 static git_repository *repo;
 
@@ -7,12 +8,14 @@ void test_repo_setters__initialize(void)
 {
 	cl_fixture_sandbox("testrepo.git");
 	cl_git_pass(git_repository_open(&repo, "testrepo.git"));
+	cl_must_pass(p_mkdir("new_workdir", 0777));
 }
 
 void test_repo_setters__cleanup(void)
 {
 	git_repository_free(repo);
 	cl_fixture_cleanup("testrepo.git");
+	cl_must_pass(p_rmdir("new_workdir"));
 }
 
 void test_repo_setters__setting_a_workdir_turns_a_bare_repository_into_a_standard_one(void)
