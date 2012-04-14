c1aefb35dd39efa0045a9925520b4715f82433e3
nulltoken <emeric.fermas@gmail.com>
2012-04-14 14:13:17 +0200

    Fix git_repository_set_index() refcount issue
    git_repository_free() calls git_index_free() if the owned index is not null.
    
    According to the doc, when setting a new index through git_repository_set_index() the caller has still to take care of releasing the index by itself.
    
    In order to cope with this, this fix makes sure the index refcount is incremented when a new repository is being plugged a new index.

--- tests-clar/repo/setters.c
+++ tests-clar/repo/setters.c
@@ -1,6 +1,7 @@
 #include "clar_libgit2.h"
 #include "buffer.h"
 #include "posix.h"
+#include "util.h"
 
 static git_repository *repo;
 
@@ -34,4 +35,25 @@ void test_repo_setters__setting_a_workdir_prettifies_its_path(void)
 	cl_git_pass(git_repository_set_workdir(repo, "./new_workdir"));
 
 	cl_assert(git__suffixcmp(git_repository_workdir(repo), "/") == 0);
+}
+
+void test_repo_setters__setting_a_new_index_on_a_repo_which_has_already_loaded_one_properly_honors_the_refcount(void)
+{
+	git_index *new_index;
+
+	cl_git_pass(git_index_open(&new_index, "./my-index"));
+	cl_assert(((git_refcount *)new_index)->refcount == 1);
+
+	git_repository_set_index(repo, new_index);
+	cl_assert(((git_refcount *)new_index)->refcount == 2);
+
+	git_repository_free(repo);
+	cl_assert(((git_refcount *)new_index)->refcount == 1);
+
+	git_index_free(new_index);
+
+	/* 
+	 * Ensure the cleanup method won't try to free the repo as it's already been taken care of
+	 */
+	repo = NULL;
 }
