bfc13e7985a9368f7932115bc3d395dd1379b03d
Ben Straub <bstraub@github.com>
2012-04-30 09:58:43 -0700

    Adding comment documentation for rev-parse api.

--- include/git2/revparse.h
+++ include/git2/revparse.h
@@ -11,12 +11,26 @@
 #include "types.h"
 
 
+/**
+ * @file git2/revparse.h
+ * @brief Git revision parsing routines
+ * @defgroup git_revparse Git revision parsing routines
+ * @ingroup Git
+ * @{
+ */
 GIT_BEGIN_DECL
 
+/**
+ * Find an object, as specified by a revision string. See `man gitrevisions`, or the documentation
+ * for `git rev-parse` for information on the syntax accepted.
+ *
+ * @param out pointer to output object
+ * @param repo the repository to search in
+ * @param spec the textual specification for an object
+ * @return  on success, GIT_ERROR otherwise (use git_error_last for information about the error)
+ */
 GIT_EXTERN(int) git_revparse_single(git_object **out, git_repository *repo, const char *spec);
 
-//GIT_EXTERN(int) git_revparse_multi(TODO);
-
+/** @} */
 GIT_END_DECL
-
 #endif
