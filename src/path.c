46811561ae19701d4d0fc4b00113667f81961dfc
nulltoken <emeric.fermas@gmail.com>
2012-05-07 13:56:42 +0200

    path: Make git_path_prettify() properly handle ENOTDIR errno value

--- src/path.c
+++ src/path.c
@@ -207,7 +207,7 @@ int git_path_prettify(git_buf *path_out, const char *path, const char *base)
 	if (p_realpath(path, buf) == NULL) {
 		giterr_set(GITERR_OS, "Failed to resolve path '%s'", path);
 		git_buf_clear(path_out);
-		return (errno == ENOENT) ? GIT_ENOTFOUND : -1;
+		return (errno == ENOENT || errno == ENOTDIR) ? GIT_ENOTFOUND : -1;
 	}
 
 	return git_buf_sets(path_out, buf);
