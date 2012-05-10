46c2ead05d289b4ed1ad96c63b75a228f02dd74c
Ben Straub <bstraub@github.com>
2012-05-10 13:39:34 -0700

    Now properly handling branches with "-g" in their names.

--- tests-clar/refs/revparse.c
+++ tests-clar/refs/revparse.c
@@ -66,6 +66,7 @@ void test_refs_revparse__partial_refs(void)
 void test_refs_revparse__describe_output(void)
 {
    test_object("blah-7-gc47800c", "c47800c7266a2be04c571c04d5a6614691ea99bd");
+   test_object("not-good", "a65fedf39aefe402d3bb6e24df4d4f5fe4547750");
 }
 
 void test_refs_revparse__nth_parent(void)
