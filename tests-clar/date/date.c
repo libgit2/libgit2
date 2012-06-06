36c0802245e2c041db19d65173587c1a39cfb742
Ben Straub <bstraub@github.com>
2012-06-06 12:39:29 -0700

    Omit failing test on 32-bit machines.
    This test is intended to verify that 64-bit
    machines can handle parsing dates in 2039 and
    beyond, and fails on 32-bit machines. It is now
    omitted when run on a 32-bit machine to eliminate
    an expected failure.

--- tests-clar/date/date.c
+++ tests-clar/date/date.c
@@ -4,10 +4,12 @@
 
 void test_date_date__overflow(void)
 {
+#ifdef __LP64__
    git_time_t d2038, d2039;
 
-   /* This fails on a 32-bit machine. */
+   /* This is expected to fail on a 32-bit machine. */
    cl_git_pass(git__date_parse(&d2038, "2038-1-1"));
    cl_git_pass(git__date_parse(&d2039, "2039-1-1"));
    cl_assert(d2038 < d2039);
+#endif
 }
