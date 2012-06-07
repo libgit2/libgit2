6b5db63c159ea1d2112fe8dc535b877da03a6848
Chris Young <chris@unsatisfactorysoftware.co.uk>
2012-06-07 21:40:07 +0100

    random page size for os4

--- src/pool.c
+++ src/pool.c
@@ -275,6 +275,8 @@ uint32_t git_pool__system_page_size(void)
 		SYSTEM_INFO info;
 		GetSystemInfo(&info);
 		size = (uint32_t)info.dwPageSize;
+#elif defined(__amigaos4__)
+		size = (uint32_t)1000000; // random value
 #else
 		size = (uint32_t)sysconf(_SC_PAGE_SIZE);
 #endif
