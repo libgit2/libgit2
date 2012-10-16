92f91b0e3bc3b7799b2b18ea07b167ad191a47a9
Ben Straub <bs@github.com>
2012-10-16 12:41:20 -0700

    Clone: fix indentation

--- include/git2/clone.h
+++ include/git2/clone.h
@@ -36,12 +36,13 @@ GIT_BEGIN_DECL
  * @return 0 on success, GIT_ERROR otherwise (use giterr_last for information
  * about the error)
  */
-GIT_EXTERN(int) git_clone(git_repository **out,
-								  const char *origin_url,
-								  const char *workdir_path,
-								  git_indexer_stats *fetch_stats,
-								  git_indexer_stats *checkout_stats,
-								  git_checkout_opts *checkout_opts);
+GIT_EXTERN(int) git_clone(
+		git_repository **out,
+		const char *origin_url,
+		const char *workdir_path,
+		git_indexer_stats *fetch_stats,
+		git_indexer_stats *checkout_stats,
+		git_checkout_opts *checkout_opts);
 
 /**
  * Create a bare clone of a remote repository.
@@ -52,10 +53,11 @@ GIT_EXTERN(int) git_clone(git_repository **out,
  * @param fetch_stats pointer to structure that receives fetch progress information (may be NULL)
  * @return 0 on success, GIT_ERROR otherwise (use giterr_last for information about the error)
  */
-GIT_EXTERN(int) git_clone_bare(git_repository **out,
-										 const char *origin_url,
-										 const char *dest_path,
-										 git_indexer_stats *fetch_stats);
+GIT_EXTERN(int) git_clone_bare(
+		git_repository **out,
+		const char *origin_url,
+		const char *dest_path,
+		git_indexer_stats *fetch_stats);
 
 /** @} */
 GIT_END_DECL
