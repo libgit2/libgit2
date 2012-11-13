0da81d2b39290fe4d444953acb6d68795ed1ef42
Carlos Martín Nieto <cmn@elego.de>
2012-11-13 14:43:23 -0800

    config: return an emtpy string when there is no value
    Returning NULL for the string when we haven't signaled an error
    condition is counter-intuitive and causes unnecessary edge
    cases. Return an empty string when asking for a string value for a
    configuration variable such as '[section] var' to avoid these edge
    cases.
    
    If the distinction between no value and an empty value is needed, this
    can be retrieved from the entry directly. As a side-effect, this
    change stops the int parsing functions from segfaulting on such a
    variable.

--- tests-clar/config/read.c
+++ tests-clar/config/read.c
@@ -87,8 +87,10 @@ void test_config_read__lone_variable(void)
 
 	cl_git_pass(git_config_open_ondisk(&cfg, cl_fixture("config/config4")));
 
+	cl_git_fail(git_config_get_int32(&i, cfg, "some.section.variable"));
+
 	cl_git_pass(git_config_get_string(&str, cfg, "some.section.variable"));
-	cl_assert(str == NULL);
+	cl_assert_equal_s(str, "");
 
 	cl_git_pass(git_config_get_bool(&i, cfg, "some.section.variable"));
 	cl_assert(i == 1);
