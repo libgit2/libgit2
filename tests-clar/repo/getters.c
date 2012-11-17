5df7207a674d1d4c41b300f6f0c5548d087f06b3
nulltoken <emeric.fermas@gmail.com>
2012-11-17 06:56:19 -0800

    repo: readonly tests don't need a sandboxed repo

--- tests-clar/repo/getters.c
+++ tests-clar/repo/getters.c
@@ -1,15 +1,5 @@
 #include "clar_libgit2.h"
 
-void test_repo_getters__initialize(void)
-{
-	cl_fixture_sandbox("testrepo.git");
-}
-
-void test_repo_getters__cleanup(void)
-{
-	cl_fixture_cleanup("testrepo.git");
-}
-
 void test_repo_getters__empty(void)
 {
 	git_repository *repo_empty, *repo_normal;
@@ -28,7 +18,7 @@ void test_repo_getters__retrieving_the_odb_honors_the_refcount(void)
 	git_odb *odb;
 	git_repository *repo;
 
-	cl_git_pass(git_repository_open(&repo, "testrepo.git"));
+	cl_git_pass(git_repository_open(&repo, cl_fixture("testrepo.git")));
 
 	cl_git_pass(git_repository_odb(&odb, repo));
 	cl_assert(((git_refcount *)odb)->refcount == 2);
