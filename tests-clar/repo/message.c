c9459abb61433ee83015c45cc30407512a0cb5af
Scott J. Goldman <scottjg@github.com>
2013-02-07 03:12:39 -0800

    tests: fix indentation in repo/message.c

--- tests-clar/repo/message.c
+++ tests-clar/repo/message.c
@@ -31,16 +31,16 @@ void test_repo_message__message(void)
 	ssize_t len;
 
 	cl_git_pass(git_buf_joinpath(&_path, git_repository_path(_repo), "MERGE_MSG"));
-        cl_git_mkfile(git_buf_cstr(&_path), expected);
+	cl_git_mkfile(git_buf_cstr(&_path), expected);
 
-        len = git_repository_message(NULL, 0, _repo);
-        cl_assert(len > 0);
-        _actual = git__malloc(len + 1);
-        cl_assert(_actual != NULL);
+	len = git_repository_message(NULL, 0, _repo);
+	cl_assert(len > 0);
+	_actual = git__malloc(len + 1);
+	cl_assert(_actual != NULL);
 
-        cl_assert(git_repository_message(_actual, len, _repo) > 0);
-        _actual[len] = '\0';
-        cl_assert_equal_s(expected, _actual);
+	cl_assert(git_repository_message(_actual, len, _repo) > 0);
+	_actual[len] = '\0';
+	cl_assert_equal_s(expected, _actual);
 
 	cl_git_pass(p_unlink(git_buf_cstr(&_path)));
 	cl_assert_equal_i(GIT_ENOTFOUND, git_repository_message(NULL, 0, _repo));
