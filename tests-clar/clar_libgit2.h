3ba01362437102501a173b9fe072a5690358baa0
Russell Belfer <rb@github.com>
2013-03-20 11:46:03 -0700

    Update cl_assert_equal_sz to be nicer
    This makes the size_t comparison test nicer (assuming that the
    values are actually not using the full length), and converts
    some cases that were using it for pointer comparison to use the
    macro that is designed for pointer comparison.

--- tests-clar/clar_libgit2.h
+++ tests-clar/clar_libgit2.h
@@ -29,7 +29,7 @@
 
 void cl_git_report_failure(int, const char *, int, const char *);
 
-#define cl_assert_equal_sz(sz1,sz2) cl_assert((sz1) == (sz2))
+#define cl_assert_equal_sz(sz1,sz2) cl_assert_equal_i((int)sz1, (int)(sz2))
 
 /*
  * Some utility macros for building long strings
