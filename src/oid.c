0c8efb38f9ffb1c4fffe620174669c51866eff79
Xavier L <xavier.l@afrosoft.tk>
2013-03-21 11:59:01 -0400

    Added an oid function that accepts nul-terminated strings

--- src/oid.c
+++ src/oid.c
@@ -51,6 +51,11 @@ int git_oid_fromstrn(git_oid *out, const char *str, size_t length)
 	return 0;
 }
 
+int git_oid_fromstrp(git_oid *out, const char *str)
+{
+	return git_oid_fromstrn(out, str, min(strlen(str), GIT_OID_HEXSZ));
+}
+
 int git_oid_fromstr(git_oid *out, const char *str)
 {
 	return git_oid_fromstrn(out, str, GIT_OID_HEXSZ);
