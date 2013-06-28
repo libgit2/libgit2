e961a62d9a0feed1df2497c013a02869673a92d2
Alan Rogers <alan@github.com>
2013-06-28 14:31:07 +1000

    We DO still need load the config from the index apparently.

--- src/submodule.c
+++ src/submodule.c
@@ -1269,7 +1269,8 @@ static int load_submodule_config(git_repository *repo)
 
 	/* add submodule information from index */
 
-	load_submodule_config_from_index(repo, &gitmodules_oid);
+    if ((error = load_submodule_config_from_index(repo, &gitmodules_oid)) < 0)        
+        goto cleanup;
 
 	/* add submodule information from HEAD */
 
