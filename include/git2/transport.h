7586f4ff0a595bf7d8dc8c520df41b328ba63cce
Etienne Samson <tiennou7@gmail.com>
2013-09-17 01:17:28 +0200

    Make `git_credtype_t` a real bitmask.

--- include/git2/transport.h
+++ include/git2/transport.h
@@ -28,11 +28,11 @@ GIT_BEGIN_DECL
  *** Begin interface for credentials acquisition ***
  */
 
+/* The authentication type requested */
 typedef enum {
-	/* git_cred_userpass_plaintext */
-	GIT_CREDTYPE_USERPASS_PLAINTEXT = 1,
-	GIT_CREDTYPE_SSH_KEYFILE_PASSPHRASE = 2,
-	GIT_CREDTYPE_SSH_PUBLICKEY = 3,
+	GIT_CREDTYPE_USERPASS_PLAINTEXT     = (1u << 0),
+	GIT_CREDTYPE_SSH_KEYFILE_PASSPHRASE = (1u << 1),
+	GIT_CREDTYPE_SSH_PUBLICKEY          = (1u << 2),
 } git_credtype_t;
 
 /* The base structure for all credential types */
