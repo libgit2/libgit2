cfd192b0140f004404a208cde967ac8eef16cdff
Ben Straub <bs@github.com>
2013-10-03 12:44:34 -0700

    Add test for multiple thread init/shutdown

--- tests-clar/threads/basic.c
+++ tests-clar/threads/basic.c
@@ -21,3 +21,16 @@ void test_threads_basic__cache(void)
 	// run several threads polling the cache at the same time
 	cl_assert(1 == 1);
 }
+
+void test_threads_basic__multiple_init(void)
+{
+	git_repository *nested_repo;
+
+	git_threads_init();
+	cl_git_pass(git_repository_open(&nested_repo, cl_fixture("testrepo.git")));
+	git_repository_free(nested_repo);
+
+	git_threads_shutdown();
+	cl_git_pass(git_repository_open(&nested_repo, cl_fixture("testrepo.git")));
+	git_repository_free(nested_repo);
+}
