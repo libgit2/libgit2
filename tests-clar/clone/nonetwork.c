887df99f17c44b0726e0034885ea922b99254933
Ben Straub <bs@github.com>
2013-10-31 13:29:16 -0700

    Test another bad URL

--- tests-clar/clone/nonetwork.c
+++ tests-clar/clone/nonetwork.c
@@ -58,7 +58,9 @@ void test_clone_nonetwork__bad_urls(void)
 	cl_git_fail(git_clone(&g_repo, "git://example.com:asdf", "./foo", &g_options));
 	cl_git_fail(git_clone(&g_repo, "https://example.com:asdf/foo", "./foo", &g_options));
 	cl_git_fail(git_clone(&g_repo, "git://github.com/git://github.com/foo/bar.git.git",
-				"./bar", &g_options));
+				"./foo", &g_options));
+	cl_git_fail(git_clone(&g_repo, "arrbee:my/bad:password@github.com:1111/strange:words.git",
+				"./foo", &g_options));
 }
 
 void test_clone_nonetwork__do_not_clean_existing_directory(void)
