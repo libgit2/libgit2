9eb45fc51a55fb8b058a28b5ee1e297991fedca5
Brodie Rao <brodie@sf.io>
2014-01-12 23:29:44 -0800

    branch: handle NULL pointers passed to git_branch_iterator_free()
    Signed-off-by: Brodie Rao <brodie@sf.io>

--- src/branch.c
+++ src/branch.c
@@ -181,6 +181,9 @@ void git_branch_iterator_free(git_branch_iterator *_iter)
 {
 	branch_iter *iter = (branch_iter *) _iter;
 
+	if (iter == NULL)
+		return;
+
 	git_reference_iterator_free(iter->iter);
 	git__free(iter);
 }
