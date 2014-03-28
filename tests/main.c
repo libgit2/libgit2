e0d61c7b1caa011ab6a7777535823fe18c50e13c
Edward Thomson <ethomson@microsoft.com>
2014-03-28 10:10:59 -0700

    Sandbox configuration during test runs

--- tests/main.c
+++ tests/main.c
@@ -6,12 +6,21 @@ int __cdecl main(int argc, char *argv[])
 int main(int argc, char *argv[])
 #endif
 {
+	const char *sandbox_path;
 	int res;
+
+	clar_test_init(argc, argv);
 
 	git_threads_init();
 
+	sandbox_path = clar_sandbox_path();
+	git_libgit2_opts(GIT_OPT_SET_SEARCH_PATH, GIT_CONFIG_LEVEL_GLOBAL, sandbox_path);
+	git_libgit2_opts(GIT_OPT_SET_SEARCH_PATH, GIT_CONFIG_LEVEL_XDG, sandbox_path);
+
 	/* Run the test suite */
-	res = clar_test(argc, argv);
+	res = clar_test_run();
+
+	clar_test_shutdown();
 
 	giterr_clear();
 	git_threads_shutdown();
