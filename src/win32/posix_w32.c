fb8581d283aed3f00df33636ff62f815aae572a3
Albert Meltzer <kitbellew@users.noreply.github.com>
2014-05-09 14:02:42 -0700

    Fix mkstemp on Win32.
    It only opened the file for writing; normally, the function is expected
    to create a file for reading as well and apply 0600 as the permissions.
    
    See, for instance:
    http://man7.org/linux/man-pages/man3/mkstemp.3.html

--- src/win32/posix_w32.c
+++ src/win32/posix_w32.c
@@ -569,7 +569,7 @@ int p_mkstemp(char *tmp_path)
 		return -1;
 #endif
 
-	return p_creat(tmp_path, 0744); //-V536
+	return p_open(tmp_path, O_RDWR | O_CREAT | O_TRUNC | O_EXCL, 0600);
 }
 
 int p_access(const char* path, mode_t mode)
