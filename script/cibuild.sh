8f426d7dd26856150950d080fb821044bb2ea811
Philip Kelley <phkelley@hotmail.com>
2014-06-09 11:43:25 -0400

    Win32: Enable WinHTTP for MinGW

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -6,17 +6,25 @@ then
 	exit $?;
 fi
 
+mkdir _build
+cd _build
+# shellcheck disable=SC2086
+cmake .. -DCMAKE_INSTALL_PREFIX=../_install $OPTIONS || exit $?
+make -j2 install || exit $?
+
+# If this platform doesn't support test execution, bail out now
+if [ -n "$SKIP_TESTS" ];
+then
+	exit $?;
+fi
+
 # Create a test repo which we can use for the online::push tests
 mkdir "$HOME"/_temp
 git init --bare "$HOME"/_temp/test.git
 git daemon --listen=localhost --export-all --enable=receive-pack --base-path="$HOME"/_temp "$HOME"/_temp 2>/dev/null &
 export GITTEST_REMOTE_URL="git://localhost/test.git"
 
-mkdir _build
-cd _build
-# shellcheck disable=SC2086
-cmake .. -DCMAKE_INSTALL_PREFIX=../_install $OPTIONS || exit $?
-make -j2 install || exit $?
+# Run the test suite
 ctest -V . || exit $?
 
 # Now that we've tested the raw git protocol, let's set up ssh to we
