8733993599d9d9a8fe1b042a1e18d6ecb9b0a0b2
Carlos Martín Nieto <cmn@dwim.me>
2014-06-26 22:38:54 +0200

    travis: enable password authentication on OSX
    Our ssh tests assume that the server supports password authentication
    in a few places. This is convenient as we're not testing authentication
    methods, but what happens around them.
    
    Tell sshd on OSX to accept this form of authentication.

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -22,7 +22,13 @@ ctest -V . || exit $?
 # can do the push tests over it
 
 killall git-daemon
-sudo start ssh
+
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    echo 'PasswordAuthentication yes' | sudo tee -a /etc/sshd_config
+else
+    sudo start ssh
+fi
+
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -N "" -q
 cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_keys
 ssh-keyscan -t rsa localhost >>~/.ssh/known_hosts
