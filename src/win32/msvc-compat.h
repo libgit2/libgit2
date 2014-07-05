cde32d4d28ae14e583853d07cdaf19ef9ee2dd25
Jacques Germishuys <jacquesg@striata.com>
2014-07-05 21:25:55 +0200

    Variadic macros is only available from Visual Studio 2005+

--- src/win32/msvc-compat.h
+++ src/win32/msvc-compat.h
@@ -38,7 +38,7 @@
 typedef SSIZE_T ssize_t;
 
 /* define snprintf using variadic macro support if available */
-#if _MSC_VER >= 1400
+#if _MSC_VER >= 1500
 # define snprintf(BUF, SZ, FMT, ...) _snprintf_s(BUF, SZ, _TRUNCATE, FMT, __VA_ARGS__)
 #else
 # define snprintf _snprintf
