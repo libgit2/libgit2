3460f2153d81688fdca99be502201b7745725034
Linquize <linquize@yahoo.com.hk>
2014-07-18 12:21:38 +0800

    Add test to test long gitlink

--- tests/repo/open.c
+++ tests/repo/open.c
@@ -74,7 +74,7 @@ static void make_gitlink_dir(const char *dir, const char *linktext)
 {
 	git_buf path = GIT_BUF_INIT;
 
-	cl_git_pass(git_futils_mkdir(dir, NULL, 0777, GIT_MKDIR_VERIFY_DIR));
+	cl_git_pass(git_futils_mkdir(dir, NULL, 0777, GIT_MKDIR_VERIFY_DIR | GIT_MKDIR_PATH));
 	cl_git_pass(git_buf_joinpath(&path, dir, ".git"));
 	cl_git_rewritefile(path.ptr, linktext);
 	git_buf_free(&path);
@@ -96,6 +96,27 @@ void test_repo_open__gitlinked(void)
 
 	cl_assert(git_repository_workdir(repo2) != NULL);
 	cl_assert_(git__suffixcmp(git_repository_workdir(repo2), "alternate/") == 0, git_repository_workdir(repo2));
+
+	git_repository_free(repo2);
+}
+
+void test_repo_open__gitlinked_long(void)
+{
+	/* need to have both repo dir and workdir set up correctly */
+	git_repository *repo = cl_git_sandbox_init("empty_standard_repo");
+	git_repository *repo2;
+
+	make_gitlink_dir("000000000/111111111/222222222/333333333/444444444"
+		, "gitdir: ../../../../../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../000000000/../empty_standard_repo/.git");
+
+	cl_git_pass(git_repository_open(&repo2, "000000000/111111111/222222222/333333333/444444444"));
+
+	cl_assert(git_repository_path(repo2) != NULL);
+	cl_assert_(git__suffixcmp(git_repository_path(repo2), "empty_standard_repo/.git/") == 0, git_repository_path(repo2));
+	cl_assert_equal_s(git_repository_path(repo), git_repository_path(repo2));
+
+	cl_assert(git_repository_workdir(repo2) != NULL);
+	cl_assert_(git__suffixcmp(git_repository_workdir(repo2), "000000000/111111111/222222222/333333333/444444444/") == 0, git_repository_workdir(repo2));
 
 	git_repository_free(repo2);
 }
