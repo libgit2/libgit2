4e53c28096f4b04ab9b573fc594a538f51e10049
Jacques Germishuys <jacquesg@striata.com>
2014-08-17 14:55:06 +0200

    Check if the refspec matches before transforming

--- src/refspec.c
+++ src/refspec.c
@@ -223,20 +223,32 @@ static int refspec_transform(
 
 int git_refspec_transform(git_buf *out, const git_refspec *spec, const char *name)
 {
-        git_buf_sanitize(out);
+	assert(out && spec && name);
+	git_buf_sanitize(out);
 
 	if (!spec->pattern)
 		return git_buf_puts(out, spec->dst);
+
+	if (!git_refspec_src_matches(spec, name)) {
+		giterr_set(GITERR_INVALID, "ref '%s' doesn't match the source", name);
+		return -1;
+	}
 
 	return refspec_transform(out, spec->src, spec->dst, name);
 }
 
 int git_refspec_rtransform(git_buf *out, const git_refspec *spec, const char *name)
 {
-        git_buf_sanitize(out);
+	assert(out && spec && name);
+	git_buf_sanitize(out);
 
 	if (!spec->pattern)
 		return git_buf_puts(out, spec->src);
+
+	if (!git_refspec_dst_matches(spec, name)) {
+		giterr_set(GITERR_INVALID, "ref '%s' doesn't match the destination", name);
+		return -1;
+	}
 
 	return refspec_transform(out, spec->dst, spec->src, name);
 }
