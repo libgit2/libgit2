e0235452a407647c7bda2e76f0f17f1a1db843e0
Carlos Martín Nieto <cmn@dwim.me>
2014-09-10 18:28:19 +0200

    global: free the error message when exiting a thread
    When we free the global state at thread termination, we must also free
    the error message in order not to leak the string once per thread.

--- tests/threads/basic.c
+++ tests/threads/basic.c
@@ -1,5 +1,6 @@
 #include "clar_libgit2.h"
 
+#include "thread_helpers.h"
 #include "cache.h"
 
 
@@ -33,4 +34,17 @@ void test_threads_basic__multiple_init(void)
 	git_threads_shutdown();
 	cl_git_pass(git_repository_open(&nested_repo, cl_fixture("testrepo.git")));
 	git_repository_free(nested_repo);
+}
+
+static void *set_error(void *dummy)
+{
+	giterr_set(GITERR_INVALID, "oh no, something happened!\n");
+
+	return dummy;
+}
+
+/* Set errors so we can check that we free it */
+void test_threads_basic__set_error(void)
+{
+	run_in_parallel(1, 4, set_error, NULL, NULL);
 }
