8f212f64a0cb01abab35b3333b0d405cb9a1639c
Carlos Martín Nieto <cmn@dwim.me>
2014-12-18 05:55:46 +0100

    commit: introduce _fromstate()
    This variant of the commit creation function takes the reference to
    update, the tree and the parents from the current branch, index and
    merging state, allowing for simpler use of a common use-case.

--- include/git2/commit.h
+++ include/git2/commit.h
@@ -443,6 +443,27 @@ GIT_EXTERN(int) git_commit_create_buffer(
 	size_t parent_count,
 	const git_commit *parents[]);
 
+/*
+ * Create a new commit from the current state and update the current branch
+ *
+ * Creates a new commit, similarly to the other functions. This
+ * variant takes the current state of the repository instead of
+ * arguments.
+ *
+ * The current branch will be updated. The tree will be created from
+ * the repository's index. The parents will be taken from HEAD and
+ * MERGE_HEAD, if applicable.
+ *
+ * @see git_commit_create
+ */
+GIT_EXTERN(int) git_commit_create_fromstate(
+	git_oid *id,
+	git_repository *repo,
+	const git_signature *author,
+	const git_signature *committer,
+	const char *message_encoding,
+	const char *message);
+
 /**
  * Create a commit object from the given buffer and signature
  *
