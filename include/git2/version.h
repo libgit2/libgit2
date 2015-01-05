9b2efc15a2d7d406fdf7a9097ddde6d4efa29ce0
Carlos Martín Nieto <cmn@dwim.me>
2015-01-05 20:16:48 +0000

    Bump version to 0.22
    Bump the version number to 0.22.0 and the SOVERSION to 22.

--- include/git2/version.h
+++ include/git2/version.h
@@ -7,11 +7,11 @@
 #ifndef INCLUDE_git_version_h__
 #define INCLUDE_git_version_h__
 
-#define LIBGIT2_VERSION "0.21.0"
+#define LIBGIT2_VERSION "0.22.0"
 #define LIBGIT2_VER_MAJOR 0
-#define LIBGIT2_VER_MINOR 21
+#define LIBGIT2_VER_MINOR 22
 #define LIBGIT2_VER_REVISION 0
 
-#define LIBGIT2_SOVERSION 21
+#define LIBGIT2_SOVERSION 22
 
 #endif
