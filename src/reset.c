2461e0d20f46b6cfaf222759aca4a993ddc7b215
Pierre-Olivier Latour <pol@mac.com>
2015-03-08 14:10:02 -0700

    Removed unnecessary GIT_CHECKOUT_SKIP_UNMERGED for GIT_RESET_HARD

--- src/reset.c
+++ src/reset.c
@@ -149,7 +149,7 @@ int git_reset(
 
 	if (reset_type == GIT_RESET_HARD) {
 		/* overwrite working directory with HEAD */
-		opts.checkout_strategy = GIT_CHECKOUT_FORCE | GIT_CHECKOUT_SKIP_UNMERGED;
+		opts.checkout_strategy = GIT_CHECKOUT_FORCE;
 
 		if ((error = git_checkout_tree(repo, (git_object *)tree, &opts)) < 0)
 			goto cleanup;
