fb6df50b7f250a4fd8b2fab257f119a5185e9bf5
Carlos Martín Nieto <cmn@dwim.me>
2015-03-24 17:10:45 +0100

    Bump version to 0.22.2

--- include/git2/version.h
+++ include/git2/version.h
@@ -7,10 +7,10 @@
 #ifndef INCLUDE_git_version_h__
 #define INCLUDE_git_version_h__
 
-#define LIBGIT2_VERSION "0.22.0"
+#define LIBGIT2_VERSION "0.22.2"
 #define LIBGIT2_VER_MAJOR 0
 #define LIBGIT2_VER_MINOR 22
-#define LIBGIT2_VER_REVISION 0
+#define LIBGIT2_VER_REVISION 2
 
 #define LIBGIT2_SOVERSION 22
 
