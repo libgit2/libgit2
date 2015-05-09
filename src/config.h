15c381031391cbc24691f9bd009b991eaa440d25
Carlos Martín Nieto <cmn@dwim.me>
2015-05-09 11:22:27 +0200

    config: provide a function to reverse-lookup mapped cvars

--- src/config.h
+++ src/config.h
@@ -82,4 +82,10 @@ extern int git_config__get_int_force(
 extern int git_config__cvar(
 	int *out, git_config *config, git_cvar_cached cvar);
 
+/**
+ * The opposite of git_config_lookup_map_value, we take an enum value
+ * and map it to the string or bool value on the config.
+ */
+int git_config_lookup_map_enum(git_cvar_t *type_out, const char **str_out,
+			       const git_cvar_map *maps, size_t map_n, int enum_val);
 #endif
