8113056c7a9e3efc543e75147fe72a25405fc9f3
Pierre-Olivier Latour <pol@mac.com>
2015-06-23 20:57:09 -0700

    Fixed Xcode 6.1 build warnings

--- tests/repo/open.c
+++ tests/repo/open.c
@@ -22,17 +22,17 @@ void test_repo_open__bare_empty_repo(void)
 
 void test_repo_open__format_version_1(void)
 {
-	git_buf path = GIT_BUF_INIT;
 	git_repository *repo;
 	git_config *config;
 
 	repo = cl_git_sandbox_init("empty_bare.git");
 
 	cl_git_pass(git_repository_open(&repo, "empty_bare.git"));
-	cl_git_pass(git_repository_config__weakptr(&config, repo));
+	cl_git_pass(git_repository_config(&config, repo));
 
 	cl_git_pass(git_config_set_int32(config, "core.repositoryformatversion", 1));
 
+	git_config_free(config);
 	git_repository_free(repo);
 	cl_git_fail(git_repository_open(&repo, "empty_bare.git"));
 }
