8a52ed7a482935c74dbb24358e21811dfa6d91c2
Carlos Martín Nieto <cmn@dwim.me>
2015-07-11 18:51:36 +0200

    errors: add EDIRECTORY
    This is to be returned when the operation which the user asked for is
    not possible to do on a directory.

--- include/git2/errors.h
+++ include/git2/errors.h
@@ -48,6 +48,7 @@ typedef enum {
 	GIT_EEOF            = -20,      /**< Unexpected EOF */
 	GIT_EINVALID        = -21,      /**< Invalid operation or input */
 	GIT_EUNCOMMITTED    = -22,	/**< Uncommitted changes in index prevented operation */
+	GIT_EDIRECTORY      = -23,      /**< The operation is not valid for a directory */
 
 	GIT_PASSTHROUGH     = -30,	/**< Internal only */
 	GIT_ITEROVER        = -31,	/**< Signals end of iteration with iterator */
