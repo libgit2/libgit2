debef9e60284bdf6e4b4f1e1c8d7a57dc77bdfb6
Keith Newton <KNEWTON11@bloomberg.net>
2015-08-05 13:40:22 -0400

    Updated http/https proxy support to work the same as git
    Proxy lookup now follows the guidelines outlined at
    http://git-scm.com/docs/git-config
    Fixed a potential memory leak in git_remote__get_http_proxy() caused by
    ce being not NULL and ce->value being NULL
    Added lookup for the ALL_PROXY environment variable
    Environment variables are now looked up before any http.* proxy entries
    per the documentation
    gitno_extract_url_parts() was not unescaping user names if a password
    was not present
    Added gitno_url_data for parsing arbitrary url's without a dependency on
    http/https connection state
    Renamed buffer_win32.c back to buffer.c, I will submit a separate pull
    request for buffer_win32.c

--- src/netops.h
+++ src/netops.h
@@ -73,6 +73,30 @@ typedef struct gitno_connection_data {
 } gitno_connection_data;
 
 /*
+ * `gitno_url_data` is very similar to `gitno_connection_data` however it includes a
+ * pointers to additional information. Parsing for `gitno_connection_data` was specifically
+ * designed with http/https connections in mind whereas this is meant to be able to
+ * work with arbitrary url's. `gitno_extract_url_parts()`, which 
+ * `gitno_connection_data_from_url()` uses for parsing, sets a default port whereas 
+ * `gitno_url_data` parsing leaves the port NULL so that the user can determine whether
+ * or not a port was present in the url. Lastly `gitno_connection_data` is already used
+ * in a number of places and there are dependencies on its `use_ssl` member variable.
+ *
+ * To summarize: `gitno_url_data` exists, despite its similarities to
+ * `gitno_connection_data`, for a separate usage case and to eliminate
+ * the possibility of introducing breaking changes to `gitno_connection_data`.
+ */
+typedef struct gitno_url_data {
+	char *scheme;
+	char *host;
+	char *port;
+	char *path;
+	char *user;
+	char *pass;
+	char *query;
+} gitno_url_data;
+
+/*
  * This replaces all the pointers in `data` with freshly-allocated strings,
  * that the caller is responsible for freeing.
  * `gitno_connection_data_free_ptrs` is good for this.
@@ -94,5 +118,31 @@ int gitno_extract_url_parts(
 		char **password,
 		const char *url,
 		const char *default_port);
+
+/*
+ * See gitno_url_data for a description of why this is different
+ * than gitno_extract_url_parts().
+ */
+int gitno_extract_url_data_parts(
+	char **scheme,
+	char **host,
+	char **port,
+	char **path,
+	char **username,
+	char **password,
+	char **query,
+	const char *url);
+
+/*
+ * This replaces all the pointers in `data` with freshly-allocated strings,
+ * that the caller is responsible for freeing.
+ * `gitno_url_data_free_ptrs` is good for this.
+ */
+int gitno_url_data_from_string(
+	gitno_url_data *data,
+	const char *url);
+
+/* This frees all the pointers IN the struct, but not the struct itself. */
+void gitno_url_data_free_ptrs(gitno_url_data *data);
 
 #endif
