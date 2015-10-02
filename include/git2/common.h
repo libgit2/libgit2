de870533e02505f3868403dabd7699da01e4ceda
Carlos Martín Nieto <cmn@dwim.me>
2015-10-02 03:43:11 +0200

    settings: add a setter for a custom user-agent

--- include/git2/common.h
+++ include/git2/common.h
@@ -145,6 +145,7 @@ typedef enum {
 	GIT_OPT_GET_TEMPLATE_PATH,
 	GIT_OPT_SET_TEMPLATE_PATH,
 	GIT_OPT_SET_SSL_CERT_LOCATIONS,
+	GIT_OPT_SET_USER_AGENT,
 } git_libgit2_opt_t;
 
 /**
@@ -239,6 +240,8 @@ typedef enum {
  *		>   certificates, one per file.
  *		>
  * 		> Either parameter may be `NULL`, but not both.
+ *
+ *	* opts(GIT_OPT_SET_USER_AGENT, const char *user_agent)
  *
  * @param option Option key
  * @param ... value to set the option
