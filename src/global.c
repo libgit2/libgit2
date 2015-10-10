c5f5a250893b82669e9f3ab3ca11d02d36804a2d
wildart <wildart@gmail.com>
2015-10-10 07:09:06 -0400

    proper certificate verification & cleanup of `git_mbedtls_stream_new`

--- src/global.c
+++ src/global.c
@@ -31,14 +31,13 @@ static git_mutex *openssl_locks;
 #ifdef GIT_MBEDTLS
 #include "mbedtls/config.h"
 #include "mbedtls/platform.h"
-#include "mbedtls/net.h"
-#include "mbedtls/debug.h"
 #include "mbedtls/ssl.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
-#include "mbedtls/error.h"
 #include "mbedtls/certs.h"
+
 #define CRT_LOC "/etc/ssl/certs"
+
 mbedtls_ssl_config *git__ssl_conf;
 mbedtls_entropy_context *mbedtls_entropy;
 #endif
