5b6af2cc2f02a8cc3acb1ad1fbc034ec4887405b
Vicent Marti <tanoku@gmail.com>
2015-10-30 11:45:52 +0100

    merge-base: Remove redundant merge bases

--- src/commit_list.h
+++ src/commit_list.h
@@ -13,6 +13,7 @@
 #define PARENT2  (1 << 1)
 #define RESULT   (1 << 2)
 #define STALE    (1 << 3)
+#define ALL_FLAGS (PARENT1 | PARENT2 | STALE | RESULT)
 
 #define PARENTS_PER_COMMIT	2
 #define COMMIT_ALLOC \
