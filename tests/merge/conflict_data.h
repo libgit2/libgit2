651bfd699c8eaa8ca36de0d0d992a1832d71df8f
Edward Thomson <ethomson@edwardthomson.com>
2015-11-09 08:24:47 -0500

    recursive: test conflict output during recursive merge

--- tests/merge/conflict_data.h
+++ tests/merge/conflict_data.h
@@ -40,3 +40,33 @@
 	"this file is changed in master and branch\n" \
 	"this file is changed in branch and master\n"
 
+#define CONFLICTING_RECURSIVE_F1_TO_F2 \
+	"VEAL SOUP.\n" \
+	"\n" \
+	"<<<<<<< HEAD\n" \
+	"PUT INTO A POT THREE QUARTS OF WATER, three onions cut small, ONE\n" \
+	"=======\n" \
+	"PUT INTO A POT THREE QUARTS OF WATER, three onions cut not too small, one\n" \
+	">>>>>>> branchF-2\n" \
+	"spoonful of black pepper pounded, and two of salt, with two or three\n" \
+	"slices of lean ham; let it boil steadily two hours; skim it\n" \
+	"occasionally, then put into it a shin of veal, let it boil two hours\n" \
+	"longer; take out the slices of ham, and skim off the grease if any\n" \
+	"should rise, take a gill of good cream, mix with it two table-spoonsful\n" \
+	"of flour very nicely, and the yelks of two eggs beaten well, strain this\n" \
+	"mixture, and add some chopped parsley; pour some soup on by degrees,\n" \
+	"stir it well, and pour it into the pot, continuing to stir until it has\n" \
+	"boiled two or three minutes to take off the raw taste of the eggs. If\n" \
+	"the cream be not perfectly sweet, and the eggs quite new, the thickening\n" \
+	"will curdle in the soup. For a change you may put a dozen ripe tomatos\n" \
+	"in, first taking off their skins, by letting them stand a few minutes in\n" \
+	"hot water, when they may be easily peeled. When made in this way you\n" \
+	"must thicken it with the flour only. Any part of the veal may be used,\n" \
+	"but the shin or knuckle is the nicest.\n" \
+	"\n" \
+	"<<<<<<< HEAD\n" \
+	"This certainly is a mighty fine recipe.\n" \
+	"=======\n" \
+	"This is a mighty fine recipe!\n" \
+	">>>>>>> branchF-2\n"
+
