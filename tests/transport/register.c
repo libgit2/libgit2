1cc7f544a005faf87f4a1d9f35bdaa7b4156c6b2
Chris Bargren <cbargren@gmail.com>
2015-12-28 11:35:19 -0700

    Adding test cases that actually test the functionality of the new transport
    ssh, ssh+git and git+ssh should all successfully build an SSH transport

--- tests/transport/register.c
+++ tests/transport/register.c
@@ -48,6 +48,9 @@ void test_transport_register__custom_transport_ssh(void)
 	cl_git_fail_with(git_transport_new(&transport, NULL, "git+ssh://somehost:somepath"), -1);
 	cl_git_fail_with(git_transport_new(&transport, NULL, "git@somehost:somepath"), -1);
 #else
+	cl_git_pass(git_transport_new(&transport, NULL, "ssh://somehost:somepath"));
+	cl_git_pass(git_transport_new(&transport, NULL, "ssh+git://somehost:somepath"));
+	cl_git_pass(git_transport_new(&transport, NULL, "git+ssh://somehost:somepath"));
 	cl_git_pass(git_transport_new(&transport, NULL, "git@somehost:somepath"));
 	transport->free(transport);
 #endif
@@ -66,6 +69,9 @@ void test_transport_register__custom_transport_ssh(void)
 	cl_git_fail_with(git_transport_new(&transport, NULL, "git+ssh://somehost:somepath"), -1);
 	cl_git_fail_with(git_transport_new(&transport, NULL, "git@somehost:somepath"), -1);
 #else
+	cl_git_pass(git_transport_new(&transport, NULL, "ssh://somehost:somepath"));
+	cl_git_pass(git_transport_new(&transport, NULL, "ssh+git://somehost:somepath"));
+	cl_git_pass(git_transport_new(&transport, NULL, "git+ssh://somehost:somepath"));
 	cl_git_pass(git_transport_new(&transport, NULL, "git@somehost:somepath"));
 	transport->free(transport);
 #endif
