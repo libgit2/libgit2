032daf4c93e3c2e528ff5396cc28a931803a2144
Carlos Martín Nieto <cmn@dwim.me>
2016-04-05 16:07:12 -0400

    wip

--- tests/core/cancellable.c
+++ tests/core/cancellable.c
@@ -16,7 +16,7 @@ void test_core_cancellable__can_cancel(void)
 	git_cancellable_source_free(cs);
 }
 
-void cancel_second(git_cancellable *cancellable, void *payload)
+static void cancel_second(git_cancellable *cancellable, void *payload)
 {
 	git_cancellable_source *cs;
 
@@ -38,12 +38,49 @@ void test_core_cancellable__can_register(void)
 	token2 = git_cancellable_source_token(cs2);
 
 	cl_git_pass(git_cancellable_register(token1, cancel_second, cs2));
+
 	cl_assert(!git_cancellable_is_cancelled(token1));
 	cl_assert(!git_cancellable_is_cancelled(token2));
+
 	git_cancellable_source_cancel(cs1);
+
 	cl_assert(git_cancellable_is_cancelled(token1));
 	cl_assert(git_cancellable_is_cancelled(token2));
 
 	git_cancellable_source_free(cs1);
 	git_cancellable_source_free(cs2);
 }
+
+static void cancel_count(git_cancellable *cancellable, void *payload)
+{
+	int *i;
+	GIT_UNUSED(cancellable);
+
+	i = (int *) payload;
+	*i += 1;
+}
+
+void test_core_cancellable__registration_fires_once(void)
+{
+	git_cancellable_source *cs;
+	git_cancellable *token;
+	int cancelled_times = 0;
+
+	cl_git_pass(git_cancellable_source_new(&cs));
+
+	token = git_cancellable_source_token(cs);
+	cl_git_pass(git_cancellable_register(token, cancel_count, &cancelled_times));
+
+	cl_assert(!git_cancellable_is_cancelled(token));
+
+	git_cancellable_source_cancel(cs);
+	cl_assert(git_cancellable_is_cancelled(token));
+
+	git_cancellable_source_cancel(cs);
+	cl_assert(git_cancellable_is_cancelled(token));
+
+	cl_assert_equal_i(1, cancelled_times);
+
+	git_cancellable_source_free(cs);
+}
+
