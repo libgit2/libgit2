70b9b8417910c97307e1c5dd5886ce76e68e201c
Krishna Ram Prakash R <krp@gtux.in>
2016-06-28 20:19:52 +0530

    Fixed bug while parsing INT64_MIN

--- tests/core/strtol.c
+++ tests/core/strtol.c
@@ -33,5 +33,13 @@ void test_core_strtol__int64(void)
 	cl_assert(i == 2147483657LL);
 	cl_git_pass(git__strtol64(&i, "  -2147483657 ", NULL, 10));
 	cl_assert(i == -2147483657LL);
+	cl_git_pass(git__strtol64(&i, " 9223372036854775807  ", NULL, 10));
+	cl_assert(i == INT64_MAX);
+	cl_git_pass(git__strtol64(&i, "   -9223372036854775808  ", NULL, 10));
+	cl_assert(i == INT64_MIN);
+	cl_git_pass(git__strtol64(&i, "   0x7fffffffffffffff  ", NULL, 16));
+	cl_assert(i == INT64_MAX);
+	cl_git_pass(git__strtol64(&i, "   -0x8000000000000000   ", NULL, 16));
+	cl_assert(i == INT64_MIN);
 }
 
