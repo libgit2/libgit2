b234b7de6b8c5582db334bc13bd12fb23fc72a34
Etienne Samson <samson.etienne@gmail.com>
2016-07-05 12:14:44 +0200

    Add a test for the io variant

--- tests/hook/execute.c
+++ tests/hook/execute.c
@@ -32,3 +32,35 @@ void test_hook_execute__hook_called(void)
 	cl_must_pass(git_hook_execute(g_repo, "post-merge", "1", NULL));
 	cl_assert_equal_i_(hook_called, 1, "hook wasn't called");
 }
+
+static int hook_exec_2(git_hook_env env, void *payload)
+{
+	int *hook_called = payload;
+
+	cl_assert_equal_i(env.args.count, 0);
+	cl_assert_equal_s(git_buf_cstr(env.io), "input-data");
+
+	*hook_called = 1;
+
+	git_buf_clear(env.io);
+	git_buf_puts(env.io, "output-data");
+
+	return 0;
+}
+
+void test_hook_execute__hook_called_with_io(void)
+{
+	int hook_called = 0;
+	git_buf input_data;
+
+	git_buf_init(&input_data, 0);
+	git_buf_puts(&input_data, "input-data");
+
+	cl_must_pass(git_hook_register_callback(g_repo, hook_exec_2, &hook_called));
+	cl_must_pass(git_hook_execute_io(&input_data, g_repo, "post-merge", NULL));
+
+	cl_assert_equal_i_(hook_called, 1, "hook wasn't called");
+	cl_assert_equal_s(git_buf_cstr(&input_data), "output-data");
+
+	git_buf_free(&input_data);
+}
