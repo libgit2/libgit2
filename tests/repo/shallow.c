b3796e9108c14cbe9bcab23409190e22a0fa5563
Etienne Samson <samson.etienne@gmail.com>
2016-07-07 20:20:29 +0200

    Test that the cache invalidation works

--- tests/repo/shallow.c
+++ tests/repo/shallow.c
@@ -52,3 +52,26 @@ void test_repo_shallow__shallow_oids(void)
 	cl_git_pass(git_repository_shallow_roots(&oids2, g_repo));
 	cl_assert_equal_p(oids.ids, oids2.ids);
 }
+
+void test_repo_shallow__cache_clearing(void)
+{
+	git_oidarray oids, oids2;
+	git_oid tmp_oid;
+
+	git_oid_fromstr(&tmp_oid, "0000000000000000000000000000000000000000");
+	g_repo = cl_git_sandbox_init("shallow.git");
+
+	cl_git_pass(git_repository_shallow_roots(&oids, g_repo));
+	cl_assert_equal_i(1, oids.count);
+	cl_assert_equal_oid(&g_shallow_oid, &oids.ids[0]);
+
+	cl_git_mkfile("shallow.git/shallow",
+		"be3563ae3f795b2b4353bcce3a527ad0a4f7f644\n"
+		"0000000000000000000000000000000000000000\n"
+	);
+
+	cl_git_pass(git_repository_shallow_roots(&oids2, g_repo));
+	cl_assert_equal_i(2, oids2.count);
+	cl_assert_equal_oid(&g_shallow_oid, &oids2.ids[0]);
+	cl_assert_equal_oid(&tmp_oid, &oids2.ids[1]);
+}
