52f9e383563d0aade045116311ff3eea3783fbc8
Etienne Samson <samson.etienne@gmail.com>
2016-07-07 20:20:14 +0200

    Make the shallow.git shallow oid a global

--- tests/repo/shallow.c
+++ tests/repo/shallow.c
@@ -2,9 +2,11 @@
 #include "fileops.h"
 
 static git_repository *g_repo;
+static git_oid g_shallow_oid;
 
 void test_repo_shallow__initialize(void)
 {
+	cl_git_pass(git_oid_fromstr(&g_shallow_oid, "be3563ae3f795b2b4353bcce3a527ad0a4f7f644"));
 }
 
 void test_repo_shallow__cleanup(void)
@@ -41,13 +43,11 @@ void test_repo_shallow__clears_errors(void)
 void test_repo_shallow__shallow_oids(void)
 {
 	git_oidarray oids, oids2;
-	git_oid oid0;
 	g_repo = cl_git_sandbox_init("shallow.git");
-	cl_git_pass(git_oid_fromstr(&oid0, "be3563ae3f795b2b4353bcce3a527ad0a4f7f644"));
 
 	cl_git_pass(git_repository_shallow_roots(&oids, g_repo));
 	cl_assert_equal_i(1, oids.count);
-	cl_assert_equal_oid(&oid0, &oids.ids[0]);
+	cl_assert_equal_oid(&g_shallow_oid, &oids.ids[0]);
 
 	cl_git_pass(git_repository_shallow_roots(&oids2, g_repo));
 	cl_assert_equal_p(oids.ids, oids2.ids);
