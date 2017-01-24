638416eb5e1b20962b583891988aed27d51f713a
Patrick Steinhardt <ps@pks.im>
2017-01-24 12:50:35 +0100

    tests: checkout: tree: fix memory leak

--- tests/checkout/tree.c
+++ tests/checkout/tree.c
@@ -435,6 +435,7 @@ void test_checkout_tree__pathlist_checkout_ignores_non_matches(void)
 
 	cl_git_pass(git_checkout_tree(g_repo, g_object, &g_opts));
 	cl_git_pass(git_repository_set_head(g_repo, "refs/heads/master"));
+	git_object_free(g_object);
 
 	cl_assert(git_path_exists("testrepo/README"));
 	cl_assert(git_path_exists("testrepo/branch_file.txt"));
@@ -454,10 +455,6 @@ void test_checkout_tree__pathlist_checkout_ignores_non_matches(void)
 	cl_assert(!git_path_exists("testrepo/branch_file.txt"));
 	cl_assert(!git_path_exists("testrepo/link_to_new.txt"));
 	cl_assert(git_path_exists("testrepo/new.txt"));
-
-	git_object_free(g_object);
-	g_object = NULL;
-
 }
 
 void test_checkout_tree__can_disable_pattern_match(void)
