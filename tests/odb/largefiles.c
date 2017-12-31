909a19921bb25444c41ae1a38a06b9e1e371bf13
Edward Thomson <ethomson@edwardthomson.com>
2017-12-31 09:56:30 -0600

    odb_loose: largefile tests only on 64 bit platforms
    Only run the large file tests on 64 bit platforms.
    
    Even though we support streaming reads on objects, and do not need to
    fit them in memory, we use `size_t` in various places to reflect the
    size of an object.

--- tests/odb/largefiles.c
+++ tests/odb/largefiles.c
@@ -67,6 +67,10 @@ void test_odb_largefiles__streamwrite(void)
 {
 	git_oid expected, oid;
 
+#ifndef GIT_ARCH_64
+	cl_skip();
+#endif
+
 	if (!cl_is_env_set("GITTEST_INVASIVE_FS_SIZE") ||
 		!cl_is_env_set("GITTEST_SLOW"))
 		cl_skip();
@@ -87,6 +91,10 @@ void test_odb_largefiles__streamread(void)
 	git_hash_ctx hash;
 	git_otype type;
 	int hdr_len, ret;
+
+#ifndef GIT_ARCH_64
+	cl_skip();
+#endif
 
 	if (!cl_is_env_set("GITTEST_INVASIVE_FS_SIZE") ||
 		!cl_is_env_set("GITTEST_SLOW"))
@@ -165,7 +173,12 @@ void test_odb_largefiles__read_header(void)
 	size_t len;
 	git_otype type;
 
-	if (!cl_is_env_set("GITTEST_INVASIVE_FS_SIZE"))
+#ifndef GIT_ARCH_64
+	cl_skip();
+#endif
+
+	if (!cl_is_env_set("GITTEST_INVASIVE_FS_SIZE") ||
+		!cl_is_env_set("GITTEST_SLOW"))
 		cl_skip();
 
 	writefile(&oid);
