f0f81f09a1cd4d6563bdce73ef50005f04b6e578
Tyrie Vella <tyrielv@gmail.com>
2018-01-17 12:23:00 -0800

    Fix base-finding for virtual commits in recursive merge

--- src/merge.c
+++ src/merge.c
@@ -2148,9 +2148,13 @@ static int compute_base(
 
 	if (given_opts)
 		memcpy(&opts, given_opts, sizeof(git_merge_options));
-
-	if ((error = insert_head_ids(&head_ids, one)) < 0 ||
-		(error = insert_head_ids(&head_ids, two)) < 0)
+	/* With more than two commits, merge_bases_many finds the base of
+	the first commit and a hypothetical merge of the others. Since "one"
+	may be a virtual commit, which insert_head_ids substitutes multiple
+	ancestors for, it needs to be added after "two" which is always a single
+	real commit. */
+	if ((error = insert_head_ids(&head_ids, two)) < 0 ||
+		(error = insert_head_ids(&head_ids, one)) < 0)
 		goto done;
 
 	if ((error = git_merge_bases_many(&bases, repo,
