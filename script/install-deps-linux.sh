cb2da47e56159faaaf143943c74ffb8f60a988b1
Etienne Samson <samson.etienne@gmail.com>
2018-03-29 22:14:17 +0200

    travis: pass -fPIC when configuring mbedtls

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -5,7 +5,8 @@ set -x
 if [ "$MBEDTLS" ]; then
   git clone --depth 10 --single-branch --branch mbedtls-2.6.1 https://github.com/ARMmbed/mbedtls.git ./deps/mbedtls
   cd ./deps/mbedtls
-  cmake -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF -DUSE_SHARED_MBEDTLS_LIBRARY=ON -DUSE_STATIC_MBEDTLS_LIBRARY=OFF .
+  # We pass -fPIC explicitely because we'll include it in libgit2.so
+  CFLAGS=-fPIC cmake -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF -DUSE_SHARED_MBEDTLS_LIBRARY=OFF -DUSE_STATIC_MBEDTLS_LIBRARY=ON .
   cmake --build .
 
   echo "mbedTLS built in `pwd`"
