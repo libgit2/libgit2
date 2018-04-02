0f09d9f554e4aed0618585c97f1c3e05367f38f2
Bernard Spil <brnrd@FreeBSD.org>
2018-04-02 20:00:07 +0200

    Fix build with LibreSSL 2.7
    LibreSSL 2.7 adds OpenSSL 1.1 API
    
    Signed-off-by: Bernard Spil <brnrd@FreeBSD.org>

--- src/streams/openssl.c
+++ src/streams/openssl.c
@@ -104,7 +104,8 @@ int git_openssl_stream_global_init(void)
 	ssl_opts |= SSL_OP_NO_COMPRESSION;
 #endif
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || \
+    (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
 	SSL_load_error_strings();
 	OpenSSL_add_ssl_algorithms();
 #else
