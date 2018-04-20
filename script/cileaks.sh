84656a18f9ce6444e13f921faa5943a8869fc2e6
Etienne Samson <samson.etienne@gmail.com>
2018-04-20 23:11:27 +0200

    scripts: use leaks on macOS
    (cherry picked from commit 4c969618f6ec6caa8facd199c3a6de0e6b06396f)

--- script/cileaks.sh
+++ script/cileaks.sh
@@ -10,4 +10,6 @@ fi
 
 if [ -n "$VALGRIND" -a -e "$(which valgrind)" ]; then
 	valgrind --leak-check=full --show-reachable=yes --error-exitcode=125 --num-callers=50 --suppressions=./libgit2_clar.supp _build/libgit2_clar $@ -ionline -xbuf::oom
+elif [ -n "$LEAKS" -a -e "$(which leaks)" ]; then
+	MallocStackLogging=1 MallocScribble=1 leaks -atExit -- _build/libgit2_clar -ionline
 fi
