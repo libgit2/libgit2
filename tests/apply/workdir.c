35d525b0d1672e64675a5e2b309bfa2ed4217e57
Edward Thomson <ethomson@edwardthomson.com>
2018-06-26 09:19:12 +0100

    apply: test that failures don't dirty workdir
    Ensure that when a patch application fails (due to a conflict in the
    working directory, for example) that we do not half-apply the patch or
    otherwise leave the working directory dirty.
    
    This is rather obvious in our current apply implementation (we do a two
    step process: one to create the post-image and one to check it out) but
    this test is a safety net for future refactoring or improvements.

--- tests/apply/workdir.c
+++ tests/apply/workdir.c
@@ -338,3 +338,32 @@ void test_apply_workdir__modified_index_with_unmodified_workdir_is_ok(void)
 	git_index_free(index);
 	git_diff_free(diff);
 }
+
+void test_apply_workdir__application_failure_leaves_workdir_unmodified(void)
+{
+	git_diff *diff;
+
+	const char *diff_file = DIFF_MODIFY_TWO_FILES;
+
+	struct merge_index_entry workdir_expected[] = {
+		{ 0100644, "f51658077d85f2264fa179b4d0848268cb3475c3", 0, "asparagus.txt" },
+		{ 0100644, "68f6182f4c85d39e1309d97c7e456156dc9c0096", 0, "beef.txt" },
+		{ 0100644, "4b7c5650008b2e747fe1809eeb5a1dde0e80850a", 0, "bouilli.txt" },
+		{ 0100644, "c4e6cca3ec6ae0148ed231f97257df8c311e015f", 0, "gravy.txt" },
+		{ 0100644, "68af1fc7407fd9addf1701a87eb1c95c7494c598", 0, "oyster.txt" },
+		{ 0100644, "8684724651336001c5dbce74bed6736d2443958d", 0, "veal.txt" },
+	};
+	size_t workdir_expected_cnt = sizeof(workdir_expected) /
+		sizeof(struct merge_index_entry);
+
+	/* mutate the workdir */
+	cl_git_rewritefile("merge-recursive/veal.txt",
+	    "This is a modification.\n");
+
+	cl_git_pass(git_diff_from_buffer(&diff, diff_file, strlen(diff_file)));
+	cl_git_fail_with(GIT_EAPPLYFAIL, git_apply(repo, diff, NULL));
+
+	validate_apply_workdir(repo, workdir_expected, workdir_expected_cnt);
+
+	git_diff_free(diff);
+}
