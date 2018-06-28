d7090ee4a8efe29fb32a1597cf128ce9c1a644c1
Edward Thomson <ethomson@edwardthomson.com>
2018-06-28 17:26:24 +0100

    apply tests: ensure we can add and remove files from the index
    Add a test that adds a new file, and another that removes a file when
    applying using `GIT_APPLY_LOCATION_INDEX` to ensure that they work.

--- tests/apply/index.c
+++ tests/apply/index.c
@@ -93,3 +93,59 @@ void test_apply_index__parsed_diff(void)
 
 	git_diff_free(diff);
 }
+
+void test_apply_index__removes_file(void)
+{
+	git_diff *diff;
+	git_apply_options opts = GIT_APPLY_OPTIONS_INIT;
+
+	struct merge_index_entry index_expected[] = {
+		{ 0100644, "f51658077d85f2264fa179b4d0848268cb3475c3", 0, "asparagus.txt" },
+		{ 0100644, "68f6182f4c85d39e1309d97c7e456156dc9c0096", 0, "beef.txt" },
+		{ 0100644, "4b7c5650008b2e747fe1809eeb5a1dde0e80850a", 0, "bouilli.txt" },
+		{ 0100644, "68af1fc7407fd9addf1701a87eb1c95c7494c598", 0, "oyster.txt" },
+		{ 0100644, "94d2c01087f48213bd157222d54edfefd77c9bba", 0, "veal.txt" },
+	};
+	size_t index_expected_cnt = sizeof(index_expected) /
+	    sizeof(struct merge_index_entry);
+
+	cl_git_pass(git_diff_from_buffer(&diff, DIFF_DELETE_FILE,
+		strlen(DIFF_DELETE_FILE)));
+
+	opts.location = GIT_APPLY_LOCATION_INDEX;
+	cl_git_pass(git_apply(repo, diff, &opts));
+
+	validate_apply_index(repo, index_expected, index_expected_cnt);
+	validate_workdir_unchanged(repo);
+
+	git_diff_free(diff);
+}
+
+void test_apply_index__adds_file(void)
+{
+	git_diff *diff;
+	git_apply_options opts = GIT_APPLY_OPTIONS_INIT;
+
+	struct merge_index_entry index_expected[] = {
+		{ 0100644, "f51658077d85f2264fa179b4d0848268cb3475c3", 0, "asparagus.txt" },
+		{ 0100644, "68f6182f4c85d39e1309d97c7e456156dc9c0096", 0, "beef.txt" },
+		{ 0100644, "4b7c5650008b2e747fe1809eeb5a1dde0e80850a", 0, "bouilli.txt" },
+		{ 0100644, "c4e6cca3ec6ae0148ed231f97257df8c311e015f", 0, "gravy.txt" },
+		{ 0100644, "6370543fcfedb3e6516ec53b06158f3687dc1447", 0, "newfile.txt" },
+		{ 0100644, "68af1fc7407fd9addf1701a87eb1c95c7494c598", 0, "oyster.txt" },
+		{ 0100644, "94d2c01087f48213bd157222d54edfefd77c9bba", 0, "veal.txt" },
+	};
+	size_t index_expected_cnt = sizeof(index_expected) /
+	    sizeof(struct merge_index_entry);
+
+	cl_git_pass(git_diff_from_buffer(&diff,
+		DIFF_ADD_FILE, strlen(DIFF_ADD_FILE)));
+
+	opts.location = GIT_APPLY_LOCATION_INDEX;
+	cl_git_pass(git_apply(repo, diff, &opts));
+
+	validate_apply_index(repo, index_expected, index_expected_cnt);
+	validate_workdir_unchanged(repo);
+
+	git_diff_free(diff);
+}
