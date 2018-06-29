771bd81e6ee3b3c04a214aacfcf8a3d2910f6b49
Edward Thomson <ethomson@edwardthomson.com>
2018-06-29 12:40:16 +0100

    apply tests: ensure apply failures leave index unmodified

--- tests/apply/index.c
+++ tests/apply/index.c
@@ -192,3 +192,37 @@ void test_apply_index__modified_workdir_with_unmodified_index_is_ok(void)
 
 	git_diff_free(diff);
 }
+
+void test_apply_index__application_failure_leaves_index_unmodified(void)
+{
+	git_diff *diff;
+	git_index *index;
+	git_apply_options opts = GIT_APPLY_OPTIONS_INIT;
+
+	const char *diff_file = DIFF_MODIFY_TWO_FILES;
+
+	struct merge_index_entry index_expected[] = {
+		{ 0100644, "f51658077d85f2264fa179b4d0848268cb3475c3", 0, "asparagus.txt" },
+		{ 0100644, "68f6182f4c85d39e1309d97c7e456156dc9c0096", 0, "beef.txt" },
+		{ 0100644, "4b7c5650008b2e747fe1809eeb5a1dde0e80850a", 0, "bouilli.txt" },
+		{ 0100644, "c4e6cca3ec6ae0148ed231f97257df8c311e015f", 0, "gravy.txt" },
+		{ 0100644, "68af1fc7407fd9addf1701a87eb1c95c7494c598", 0, "oyster.txt" },
+	};
+	size_t index_expected_cnt = sizeof(index_expected) /
+		sizeof(struct merge_index_entry);
+
+	/* mutate the index */
+	cl_git_pass(git_repository_index(&index, repo));
+	cl_git_pass(git_index_remove(index, "veal.txt", 0));
+	cl_git_pass(git_index_write(index));
+	git_index_free(index);
+
+	cl_git_pass(git_diff_from_buffer(&diff, diff_file, strlen(diff_file)));
+
+	opts.location = GIT_APPLY_LOCATION_INDEX;
+	cl_git_fail_with(GIT_EAPPLYFAIL, git_apply(repo, diff, &opts));
+
+	validate_apply_index(repo, index_expected, index_expected_cnt);
+
+	git_diff_free(diff);
+}
