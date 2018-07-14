e0ad161403c05198278e9bdfe03e7aa21a149844
Edward Thomson <ethomson@edwardthomson.com>
2018-07-14 12:34:05 +0100

    ci: setup a linux host
    Sets up a linux host to prepare for a build.
    
    (cherry picked from commit 5bb2087b7c60da5c2ce50b9eefeebfbe255c9a0d)

---
+++ ci/setup-linux.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -x
+
+apt-get update
+apt-get -y install build-essential pkg-config cmake openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
+
+mkdir -p /var/run/sshd
