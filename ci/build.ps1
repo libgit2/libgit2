7690239f3d6a05babab07608a16ae0b3e984c6b6
Edward Thomson <ethomson@edwardthomson.com>
2018-07-20 18:09:38 -0700

    ci: msvc leak-checking

--- ci/build.ps1
+++ ci/build.ps1
@@ -11,15 +11,14 @@ Write-Host "Build directory:  ${BuildDirectory}"
 Write-Host ""
 Write-Host "Operating system version:"
 Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, ServicePackMajorVersion, BuildNumber, OSArchitecture | Format-List
-Write-Host "PATH:"
-Write-Host "${Env:PATH}"
+Write-Host "PATH: ${Env:PATH}"
 Write-Host ""
 
 Write-Host "##############################################################################"
 Write-Host "## Configuring build environment"
 Write-Host "##############################################################################"
 
-cmake $SourceDirectory -DBUILD_EXAMPLES=ON ${Env:CMAKE_OPTIONS}
+Invoke-Expression "cmake ${SourceDirectory} -DBUILD_EXAMPLES=ON ${Env:CMAKE_OPTIONS}"
 if ($LastExitCode -ne 0) { [Environment]::Exit($LastExitCode) }
 
 Write-Host ""
