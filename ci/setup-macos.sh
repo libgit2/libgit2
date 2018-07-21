db7586b384b7390e19cfd9110a5fe6b7bfb6456a
Edward Thomson <ethomson@edwardthomson.com>
2018-07-20 19:47:40 -0700

    ci: enable leak checking on osx
    (cherry picked from commit b00672b9e404adb771601408d4b02711085d6f90)

--- ci/setup-macos.sh
+++ ci/setup-macos.sh
@@ -4,3 +4,5 @@ set -x
 
 brew update
 brew install pkgconfig zlib curl openssl libssh2
+
+ln -s /Applications/Xcode.app/Contents/Developer/usr/lib/libLeaksAtExit.dylib /usr/local/lib
--- script/install-deps-osx.sh
+++ script/install-deps-osx.sh
@@ -7,3 +7,5 @@ brew install zlib
 brew install curl
 brew install openssl
 brew install libssh2
+
+ln -s /Applications/Xcode.app/Contents/Developer/usr/lib/libLeaksAtExit.dylib /usr/local/lib
