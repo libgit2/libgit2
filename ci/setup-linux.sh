2e30769ca89643e4a30237d4936cbee0feed821a
Edward Thomson <ethomson@edwardthomson.com>
2018-07-26 14:04:19 +0100

    ci: perform clang builds on Linux

--- ci/setup-linux.sh
+++ ci/setup-linux.sh
@@ -3,6 +3,6 @@
 set -x
 
 apt-get update
-apt-get -y install build-essential pkg-config cmake openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
+apt-get -y install build-essential pkg-config clang cmake openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
 
 mkdir -p /var/run/sshd
