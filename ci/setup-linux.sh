6f8cc9b5a3982ecc3f8907fcf40e658d36f8da67
Edward Thomson <ethomson@edwardthomson.com>
2018-07-27 12:31:32 +0100

    ci: move travis to the new scripts
    (cherry picked from commit 24b8dd8275adb13acc68281c200623f636690666)

--- ci/setup-linux.sh
+++ ci/setup-linux.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+set -e
 set -x
 
-apt-get update
-apt-get -y install build-essential pkg-config clang cmake openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
+TMPDIR=${TMPDIR:-/tmp}
+
+if [ -z "$SKIP_APT" ]; then
+	apt-get update
+	apt-get -y install build-essential pkg-config clang cmake openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
+fi
 
 mkdir -p /var/run/sshd
