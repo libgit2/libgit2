b8d4578abbc194afa63e15aa10665b9d3aa9c921
Nelson Elhage <nelhage@nelhage.com>
2018-08-14 04:01:30 +0000

    Add a copyright header.

--- fuzzers/config_file_fuzzer.c
+++ fuzzers/config_file_fuzzer.c
@@ -1,3 +1,12 @@
+/*
+ * libgit2 config file parser fuzz target.
+ *
+ * Copyright (C) the libgit2 contributors. All rights reserved.
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
+
 #include <git2.h>
 
 #include <stdlib.h>
