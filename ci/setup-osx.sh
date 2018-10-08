6a67e42da7ee67cc55b5d3057d8e5a5d8eeddfcc
Etienne Samson <samson.etienne@gmail.com>
2018-10-08 19:33:27 +0200

    ci: use Ninja on macOS

--- ci/setup-osx.sh
+++ ci/setup-osx.sh
@@ -3,6 +3,6 @@
 set -x
 
 brew update
-brew install pkgconfig zlib curl openssl libssh2
+brew install pkgconfig zlib curl openssl libssh2 ninja
 
 ln -s /Applications/Xcode.app/Contents/Developer/usr/lib/libLeaksAtExit.dylib /usr/local/lib
