3fe29c4d4cf8f7e6ea4dc0596fd80201b2953505
Edward Thomson <ethomson@edwardthomson.com>
2019-01-31 19:10:03 +0000

    version: 0.28

--- include/git2/version.h
+++ include/git2/version.h
@@ -7,12 +7,12 @@
 #ifndef INCLUDE_git_version_h__
 #define INCLUDE_git_version_h__
 
-#define LIBGIT2_VERSION "0.27.0"
+#define LIBGIT2_VERSION "0.28.0"
 #define LIBGIT2_VER_MAJOR 0
-#define LIBGIT2_VER_MINOR 27
+#define LIBGIT2_VER_MINOR 28
 #define LIBGIT2_VER_REVISION 0
 #define LIBGIT2_VER_PATCH 0
 
-#define LIBGIT2_SOVERSION 27
+#define LIBGIT2_SOVERSION 28
 
 #endif
