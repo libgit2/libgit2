4ec209cd753208ae745b7d9227a3752db950aca6
Erik Aigner <aigner.erik@gmail.com>
2019-03-29 12:30:37 +0100

    tests: diff: test parsing diffs with a new file with spaces in its path
    Add a test that verifies that we are able to parse patches which add a
    new file that has spaces in its path.

--- tests/patch/patch_common.h
+++ tests/patch/patch_common.h
@@ -841,3 +841,12 @@
 	"diff --git a/binary.bin b/binary.bin\n" \
 	"index 27184d9..7c94f9e 100644\n" \
 	"Binary files a/binary.bin and b/binary.bin differ\n"
+
+#define PATCH_ORIGINAL_NEW_FILE_WITH_SPACE \
+	"diff --git a/sp ace.txt b/sp ace.txt\n" \
+	"new file mode 100644\n" \
+	"index 000000000..789819226\n" \
+	"--- /dev/null\n" \
+	"+++ b/sp ace.txt\n" \
+	"@@ -0,0 +1 @@\n" \
+	"+a\n"
