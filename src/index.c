1856b7fb624fd762ec95fd247c49e042eb781252
romkatv <roman.perepelitsa@gmail.com>
2019-04-04 16:46:16 +0200

    don't copy index to perform a diff; scan it directly

--- src/index.c
+++ src/index.c
@@ -3589,24 +3589,18 @@ int git_index_update_all(
 
 int git_index_snapshot_new(git_vector *snap, git_index *index)
 {
-	int error;
-
 	GIT_REFCOUNT_INC(index);
 
 	git_atomic_inc(&index->readers);
 	git_vector_sort(&index->entries);
 
-	error = git_vector_dup(snap, &index->entries, index->entries._cmp);
-
-	if (error < 0)
-		git_index_snapshot_release(snap, index);
-
-	return error;
+	*snap = index->entries;
+	return 0;
 }
 
 void git_index_snapshot_release(git_vector *snap, git_index *index)
 {
-	git_vector_free(snap);
+	(void)snap;
 
 	git_atomic_dec(&index->readers);
 
