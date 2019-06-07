21ddeabe6cb4a77899a6c7348493a2b45cc82a9c
Robert Coup <robert@coup.net.nz>
2019-06-07 15:22:42 +0100

    Review fixes:
    - whitespace -> tabs
    - comment style
    - improve repo naming in merge/trees/analysis tests.

--- tests/merge/workdir/analysis.c
+++ tests/merge/workdir/analysis.c
@@ -1,5 +1,7 @@
-// NOTE: this is essentially duplicated with tests/merge/trees/analysis.c
-// You probably want to make changes to both files.
+/*
+NOTE: this is essentially duplicated with tests/merge/trees/analysis.c
+You probably want to make changes to both files.
+*/
 
 #include "clar_libgit2.h"
 #include "git2/repository.h"
@@ -18,58 +20,61 @@ void test_merge_workdir_analysis__initialize(void)
 	repo = cl_git_sandbox_init(TEST_REPO_PATH);
 	git_repository_index(&repo_index, repo);
 
-    testimpl_merge_analysis__initialize(repo, repo_index);
+	testimpl_merge_analysis__initialize(repo, repo_index);
 }
 
 void test_merge_workdir_analysis__cleanup(void)
 {
-    testimpl_merge_analysis__cleanup();
+	testimpl_merge_analysis__cleanup();
 
 	git_index_free(repo_index);
+	repo_index = NULL;
+
 	cl_git_sandbox_cleanup();
+	repo = NULL;
 }
 
 void test_merge_workdir_analysis__fastforward(void)
 {
-    testimpl_merge_analysis__fastforward();
+	testimpl_merge_analysis__fastforward();
 }
 
 void test_merge_workdir_analysis__no_fastforward(void)
 {
-    testimpl_merge_analysis__no_fastforward();
+	testimpl_merge_analysis__no_fastforward();
 }
 
 void test_merge_workdir_analysis__uptodate(void)
 {
-    testimpl_merge_analysis__uptodate();
+	testimpl_merge_analysis__uptodate();
 }
 
 void test_merge_workdir_analysis__uptodate_merging_prev_commit(void)
 {
-    testimpl_merge_analysis__uptodate_merging_prev_commit();
+	testimpl_merge_analysis__uptodate_merging_prev_commit();
 }
 
 void test_merge_workdir_analysis__unborn(void)
 {
-    testimpl_merge_analysis__unborn();
+	testimpl_merge_analysis__unborn();
 }
 
 void test_merge_workdir_analysis__fastforward_with_config_noff(void)
 {
-    testimpl_merge_analysis__fastforward_with_config_noff();
+	testimpl_merge_analysis__fastforward_with_config_noff();
 }
 
 void test_merge_workdir_analysis__no_fastforward_with_config_ffonly(void)
 {
-    testimpl_merge_analysis__no_fastforward_with_config_ffonly();
+	testimpl_merge_analysis__no_fastforward_with_config_ffonly();
 }
 
 void test_merge_workdir_analysis__between_uptodate_refs(void)
 {
-    testimpl_merge_analysis__between_uptodate_refs();
+	testimpl_merge_analysis__between_uptodate_refs();
 }
 
 void test_merge_workdir_analysis__between_noff_refs(void)
 {
-    testimpl_merge_analysis__between_noff_refs();
+	testimpl_merge_analysis__between_noff_refs();
 }
