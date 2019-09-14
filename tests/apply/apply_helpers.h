02af1fcb69b91b3532b8b253ec82cdfce17ef28d
Drew DeVault <sir@cmpwn.com>
2019-09-14 14:03:36 -0400

    apply: add GIT_APPLY_CHECK
    This adds an option which will check if a diff is applicable without
    actually applying it; equivalent to git apply --check.

--- tests/apply/apply_helpers.h
+++ tests/apply/apply_helpers.h
@@ -21,6 +21,27 @@
 	"-longer. take out the slices of ham, and skim off the grease if any\n" \
 	"+longer; take out the slices of ham, and skim off the grease if any\n"
 
+/* This is the binary equivalent of DIFF_MODIFY_TWO_FILES */
+#define DIFF_MODIFY_TWO_FILES_BINARY \
+   "diff --git a/asparagus.txt b/asparagus.txt\n" \
+   "index f51658077d85f2264fa179b4d0848268cb3475c3..ffb36e513f5fdf8a6ba850a20142676a2ac4807d 100644\n" \
+   "GIT binary patch\n" \
+   "delta 24\n" \
+   "fcmX@ja+-zTF*v|6$k9DCSRvRyG(c}7zYP-rT_OhP\n" \
+   "\n" \
+   "delta 24\n" \
+   "fcmX@ja+-zTF*v|6$k9DCSRvRyG(d49zYP-rT;T@W\n" \
+   "\n" \
+   "diff --git a/veal.txt b/veal.txt\n" \
+   "index 94d2c01087f48213bd157222d54edfefd77c9bba..a7b066537e6be7109abfe4ff97b675d4e077da20 100644\n" \
+   "GIT binary patch\n" \
+   "delta 26\n" \
+   "hcmX@kah!uI%+=9HA=p1OKyM?L03)OIW@$zpW&mXg25bNT\n" \
+   "\n" \
+   "delta 26\n" \
+   "hcmX@kah!uI%+=9HA=p1OKyf3N03)N`W@$zpW&mU#22ub3\n" \
+   "\n"
+
 #define DIFF_DELETE_FILE \
 	"diff --git a/gravy.txt b/gravy.txt\n" \
 	"deleted file mode 100644\n" \
