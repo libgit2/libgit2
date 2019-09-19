e8e8ac966326eacaa07bd992280993d30ea0407b
Patrick Steinhardt <ps@pks.im>
2019-09-19 21:11:56 +0200

    tests: online: introduce variants for cloning from major hosting sites
    Right now, we only run each clone test against a single Git provider,
    making it hard to understand which features are tested against which
    provide. Furthermore, more intricate behaviour like e.g. authentication
    is thus not thoroughly tested against the different implementations,
    which may differ slightly across hosters.
    
    Introduce test variants to start fixing this. For each provider, we will
    initialize an optional set of URLs to test against where each URL aims
    to exercise some specific behaviour. For now, we implement tests against
    GitHub, Bitbucket, Azure and GitLab. Tests against GitLab and Azure are
    completely new.
    
    The variants also uncovered behavioural inconsistencies in how Bitbucket
    handles nonexistent repositories: instead of trying to authenticate, it
    will directly return a 404 status code and thus reveal that no such repo
    exists, not even in private. We thus special-case this behaviour in our
    test suite.

--- tests/online/clone/http.c
+++ tests/online/clone/http.c
@@ -5,24 +5,75 @@
 #include "remote.h"
 #include "refs.h"
 
-#define GH_REPO_URL "http://github.com/libgit2/TestGitRepository"
-#define GH_REPO_EMPTY_URL "http://github.com/libgit2/TestEmptyRepository"
-#define GH_REPO_NONEXISTENT_URL "http://github.com/libgit2/non-existent"
-#define BB_REPO_URL "https://libgit3@bitbucket.org/libgit2/testgitrepository.git"
-#define BB_REPO_URL_WITH_PASS "https://libgit3:libgit3@bitbucket.org/libgit2/testgitrepository.git"
-#define BB_REPO_URL_WITH_WRONG_PASS "https://libgit3:wrong@bitbucket.org/libgit2/testgitrepository.git"
-#define AZURE_REPO_SPACES_URL "https://libgit2@dev.azure.com/libgit2/test/_git/spaces%20in%20the%20name"
-
 #define CLONE_PATH "./foo"
 
+static struct {
+	const char *normal;
+	const char *empty;
+	const char *nonexisting;
+	const char *authenticated;
+	const char *misauthenticated;
+	const char *spaces;
+} g_urls;
+
+static const char *g_hostname;
 static git_repository *g_repo;
 static git_clone_options g_options;
 
-void test_online_clone_http__initialize(void)
+static int bitbucket_creds(git_cred **cred, const char *url, const char *user, unsigned int allowed_types, void *payload)
+{
+	GIT_UNUSED(url); GIT_UNUSED(user); GIT_UNUSED(payload);
+	if (GIT_CREDTYPE_USERNAME & allowed_types)
+		return git_cred_username_new(cred, "libgit3");
+	if (GIT_CREDTYPE_USERPASS_PLAINTEXT & allowed_types)
+		return git_cred_userpass_plaintext_new(cred, "libgit3", "libgit3");
+	return GIT_PASSTHROUGH;
+}
+
+static void initialize(void)
 {
 	git_clone_options opts = GIT_CLONE_OPTIONS_INIT;
 	memcpy(&g_options, &opts, sizeof(g_options));
 	g_repo = NULL;
+	memset(&g_urls, 0, sizeof(g_urls));
+}
+
+void test_online_clone_http__initialize_github(void)
+{
+	initialize();
+	g_hostname = "github.com";
+	g_urls.normal = "http://github.com/libgit2/TestGitRepository";
+	g_urls.empty = "http://github.com/libgit2/TestEmptyRepository";
+	g_urls.nonexisting = "http://github.com/libgit2/non-existent";
+}
+
+void test_online_clone_http__initialize_bitbucket(void)
+{
+	initialize();
+	g_options.fetch_opts.callbacks.credentials = bitbucket_creds;
+	g_hostname = "bitbucket.org";
+	g_urls.normal = "https://bitbucket.org/libgit2/testgitrepository.git";
+	g_urls.nonexisting = "https://bitbucket.org/libgit2/nonexistent.git";
+	g_urls.authenticated = "https://libgit3:libgit3@bitbucket.org/libgit2/testgitrepository.git";
+	g_urls.misauthenticated = "https://libgit3:wrong@bitbucket.org/libgit2/testgitrepository.git";
+}
+
+void test_online_clone_http__initialize_gitlab(void)
+{
+	initialize();
+	g_hostname = "gitlab.com";
+	g_urls.normal = "https://gitlab.com/libgit2/testgitrepository.git";
+	g_urls.empty = "https://gitlab.com/libgit2/testemptyproject.git";
+	g_urls.nonexisting = "https://gitlab.com/libgit2/nonexistent.git";
+}
+
+void test_online_clone_http__initialize_azure(void)
+{
+	initialize();
+	g_hostname = "dev.azure.com";
+	g_urls.normal = "https://libgit2@dev.azure.com/libgit2/test/_git/test";
+	g_urls.empty = "https://libgit2@dev.azure.com/libgit2/test/_git/empty";
+	g_urls.spaces = "https://libgit2@dev.azure.com/libgit2/test/_git/spaces%20in%20the%20name";
 }
 
 void test_online_clone_http__cleanup(void)
@@ -36,7 +87,10 @@ void test_online_clone_http__network_full(void)
 {
 	git_remote *origin;
 
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	if (!g_urls.normal)
+		clar__skip();
+
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 	cl_assert(!git_repository_is_bare(g_repo));
 	cl_git_pass(git_remote_lookup(&origin, g_repo, "origin"));
 
@@ -49,9 +103,12 @@ void test_online_clone_http__network_bare(void)
 {
 	git_remote *origin;
 
+	if (!g_urls.normal)
+		clar__skip();
+
 	g_options.bare = true;
 
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 	cl_assert(git_repository_is_bare(g_repo));
 	cl_git_pass(git_remote_lookup(&origin, g_repo, "origin"));
 
@@ -62,7 +119,10 @@ void test_online_clone_http__empty_repository(void)
 {
 	git_reference *head;
 
-	cl_git_pass(git_clone(&g_repo, GH_REPO_EMPTY_URL, CLONE_PATH, &g_options));
+	if (!g_urls.empty)
+		clar__skip();
+
+	cl_git_pass(git_clone(&g_repo, g_urls.empty, CLONE_PATH, &g_options));
 
 	cl_assert_equal_i(true, git_repository_is_empty(g_repo));
 	cl_assert_equal_i(true, git_repository_head_unborn(g_repo));
@@ -96,13 +156,16 @@ void test_online_clone_http__can_checkout_a_cloned_repo(void)
 	bool checkout_progress_cb_was_called = false,
 		  fetch_progress_cb_was_called = false;
 
+	if (!g_urls.normal)
+		clar__skip();
+
 	g_options.checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
 	g_options.checkout_opts.progress_cb = &checkout_progress;
 	g_options.checkout_opts.progress_payload = &checkout_progress_cb_was_called;
 	g_options.fetch_opts.callbacks.transfer_progress = &fetch_progress;
 	g_options.fetch_opts.callbacks.payload = &fetch_progress_cb_was_called;
 
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 
 	cl_git_pass(git_buf_joinpath(&path, git_repository_workdir(g_repo), "master.txt"));
 	cl_assert_equal_i(true, git_path_isfile(git_buf_cstr(&path)));
@@ -135,16 +198,18 @@ static int remote_mirror_cb(git_remote **out, git_repository *repo,
 
 void test_online_clone_http__clone_mirror(void)
 {
-	git_clone_options opts = GIT_CLONE_OPTIONS_INIT;
 	bool fetch_progress_cb_was_called = false;
 	git_reference *head;
 
-	opts.fetch_opts.callbacks.transfer_progress = &fetch_progress;
-	opts.fetch_opts.callbacks.payload = &fetch_progress_cb_was_called;
-	opts.bare = true;
-	opts.remote_cb = remote_mirror_cb;
+	if (!g_urls.normal)
+		clar__skip();
 
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &opts));
+	g_options.fetch_opts.callbacks.transfer_progress = &fetch_progress;
+	g_options.fetch_opts.callbacks.payload = &fetch_progress_cb_was_called;
+	g_options.bare = true;
+	g_options.remote_cb = remote_mirror_cb;
+
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 
 	cl_git_pass(git_reference_lookup(&head, g_repo, "HEAD"));
 	cl_assert_equal_i(GIT_REFERENCE_SYMBOLIC, git_reference_type(head));
@@ -167,10 +232,13 @@ void test_online_clone_http__custom_remote_callbacks(void)
 {
 	int callcount = 0;
 
+	if (!g_urls.normal)
+		clar__skip();
+
 	g_options.fetch_opts.callbacks.update_tips = update_tips;
 	g_options.fetch_opts.callbacks.payload = &callcount;
 
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 	cl_assert(callcount > 0);
 }
 
@@ -182,23 +250,26 @@ void test_online_clone_http__custom_headers(void)
 	char *conflict = "Accept: defined-by-git";
 	char *ok = "X-Custom: this should be ok";
 
+	if (!g_urls.normal)
+		clar__skip();
+
 	g_options.fetch_opts.custom_headers.count = 1;
 
 	g_options.fetch_opts.custom_headers.strings = &empty_header;
-	cl_git_fail(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_fail(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 
 	g_options.fetch_opts.custom_headers.strings = &unnamed_header;
-	cl_git_fail(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_fail(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 
 	g_options.fetch_opts.custom_headers.strings = &newlines;
-	cl_git_fail(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_fail(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 
 	g_options.fetch_opts.custom_headers.strings = &conflict;
-	cl_git_fail(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_fail(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 
 	/* Finally, we got it right! */
 	g_options.fetch_opts.custom_headers.strings = &ok;
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 }
 
 static int cred_failure_cb(
@@ -215,8 +286,15 @@ static int cred_failure_cb(
 
 void test_online_clone_http__cred_callback_failure_return_code_is_tunnelled(void)
 {
+	if (!g_urls.nonexisting)
+		clar__skip();
 	g_options.fetch_opts.callbacks.credentials = cred_failure_cb;
-	cl_git_fail_with(-172, git_clone(&g_repo, GH_REPO_NONEXISTENT_URL, CLONE_PATH, &g_options));
+
+	/* Bitbucket actually returns a 404 before checking credentials */
+	if (!strcmp(g_hostname, "bitbucket.org"))
+		cl_git_fail_with(-1, git_clone(&g_repo, g_urls.nonexisting, CLONE_PATH, &g_options));
+	else
+		cl_git_fail_with(-172, git_clone(&g_repo, g_urls.nonexisting, CLONE_PATH, &g_options));
 }
 
 static int cred_count_calls_cb(git_cred **cred, const char *url, const char *user,
@@ -227,7 +305,7 @@ static int cred_count_calls_cb(git_cred **cred, const char *url, const char *use
 	GIT_UNUSED(url); GIT_UNUSED(user); GIT_UNUSED(allowed_types);
 
 	if (allowed_types == GIT_CREDTYPE_USERNAME)
-		return git_cred_username_new(cred, CLONE_PATH);
+		return git_cred_username_new(cred, "foo");
 
 	(*counter)++;
 
@@ -241,30 +319,27 @@ void test_online_clone_http__cred_callback_called_again_on_auth_failure(void)
 {
 	size_t counter = 0;
 
+	if (!g_urls.nonexisting)
+		clar__skip();
+
 	g_options.fetch_opts.callbacks.credentials = cred_count_calls_cb;
 	g_options.fetch_opts.callbacks.payload = &counter;
 
-	cl_git_fail_with(GIT_EUSER, git_clone(&g_repo, GH_REPO_NONEXISTENT_URL, CLONE_PATH, &g_options));
-	cl_assert_equal_i(3, counter);
+	/* Bitbucket actually returns a 404 before checking credentials */
+	if (!strcmp(g_hostname, "bitbucket.org")) {
+		cl_git_fail_with(-1, git_clone(&g_repo, g_urls.nonexisting, CLONE_PATH, &g_options));
+	} else {
+		cl_git_fail_with(GIT_EUSER, git_clone(&g_repo, g_urls.nonexisting, CLONE_PATH, &g_options));
+		cl_assert_equal_i(3, counter);
+	}
 }
 
-void test_online_clone_http__bitbucket_style(void)
+void test_online_clone_http__creds_in_url(void)
 {
-	git_cred_userpass_payload user_pass = {
-		"libgit3", "libgit3"
-	};
+	git_cred_userpass_payload user_pass = { "libgit2", "wrong" };
 
-	g_options.fetch_opts.callbacks.credentials = git_cred_userpass;
-	g_options.fetch_opts.callbacks.payload = &user_pass;
-
-	cl_git_pass(git_clone(&g_repo, BB_REPO_URL, CLONE_PATH, &g_options));
-}
-
-void test_online_clone_http__bitbucket_uses_creds_in_url(void)
-{
-	git_cred_userpass_payload user_pass = {
-		"libgit2", "wrong"
-	};
+	if (!g_urls.authenticated)
+		clar__skip();
 
 	g_options.fetch_opts.callbacks.credentials = git_cred_userpass;
 	g_options.fetch_opts.callbacks.payload = &user_pass;
@@ -273,14 +348,17 @@ void test_online_clone_http__bitbucket_uses_creds_in_url(void)
 	 * Correct user and pass are in the URL; the (incorrect) creds in
 	 * the `git_cred_userpass_payload` should be ignored.
 	 */
-	cl_git_pass(git_clone(&g_repo, BB_REPO_URL_WITH_PASS, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.authenticated, CLONE_PATH, &g_options));
 }
 
-void test_online_clone_http__bitbucket_falls_back_to_specified_creds(void)
+void test_online_clone_http__url_creds_fallback_to_explicit_creds(void)
 {
 	git_cred_userpass_payload user_pass = {
 		"libgit2", "libgit2"
 	};
+
+	if (!g_urls.misauthenticated)
+		clar__skip();
 
 	g_options.fetch_opts.callbacks.credentials = git_cred_userpass;
 	g_options.fetch_opts.callbacks.payload = &user_pass;
@@ -295,7 +373,7 @@ void test_online_clone_http__bitbucket_falls_back_to_specified_creds(void)
 	 * Incorrect user and pass are in the URL; the (correct) creds in
 	 * the `git_cred_userpass_payload` should be used as a fallback.
 	 */
-	cl_git_pass(git_clone(&g_repo, BB_REPO_URL_WITH_WRONG_PASS, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.misauthenticated, CLONE_PATH, &g_options));
 }
 
 static int cancel_at_half(const git_indexer_progress *stats, void *payload)
@@ -309,8 +387,10 @@ static int cancel_at_half(const git_indexer_progress *stats, void *payload)
 
 void test_online_clone_http__can_cancel(void)
 {
+	if (!g_urls.normal)
+		clar__skip();
 	g_options.fetch_opts.callbacks.transfer_progress = cancel_at_half;
-	cl_git_fail_with(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options), 4321);
+	cl_git_fail_with(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options), 4321);
 }
 
 static int fail_certificate_check(git_cert *cert, int valid, const char *host, void *payload)
@@ -324,8 +404,10 @@ static int fail_certificate_check(git_cert *cert, int valid, const char *host, v
 
 void test_online_clone_http__certificate_invalid(void)
 {
+	if (!g_urls.normal)
+		clar__skip();
 	g_options.fetch_opts.callbacks.certificate_check = fail_certificate_check;
-	cl_git_fail_with(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options),
+	cl_git_fail_with(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options),
 		GIT_ECERTIFICATE);
 }
 
@@ -335,25 +417,31 @@ static int succeed_certificate_check(git_cert *cert, int valid, const char *host
 	GIT_UNUSED(valid);
 	GIT_UNUSED(payload);
 
-	cl_assert_equal_s("github.com", host);
+	cl_assert_equal_s(g_hostname, host);
 
 	return 0;
 }
 
 void test_online_clone_http__certificate_valid(void)
 {
+	if (!g_urls.normal)
+		clar__skip();
 	g_options.fetch_opts.callbacks.certificate_check = succeed_certificate_check;
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 }
 
 void test_online_clone_http__start_with_http(void)
 {
+	if (!g_urls.normal)
+		clar__skip();
 	g_options.fetch_opts.callbacks.certificate_check = succeed_certificate_check;
-	cl_git_pass(git_clone(&g_repo, GH_REPO_URL, CLONE_PATH, &g_options));
+	cl_git_pass(git_clone(&g_repo, g_urls.normal, CLONE_PATH, &g_options));
 }
 
 void test_online_clone_http__path_whitespace(void)
 {
-	cl_git_pass(git_clone(&g_repo, AZURE_REPO_SPACES_URL, CLONE_PATH, &g_options));
+	if (!g_urls.spaces)
+		clar__skip();
+	cl_git_pass(git_clone(&g_repo, g_urls.spaces, CLONE_PATH, &g_options));
 	cl_assert(git_path_exists(CLONE_PATH "/master.txt"));
 }
