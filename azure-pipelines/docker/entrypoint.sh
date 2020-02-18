01a8340662749943f3917505dc8ca65006495bec
Patrick Steinhardt <ps@pks.im>
2020-02-18 15:20:43 +0100

    azure: docker: fix ARM builds by replacing gosu(1)
    Our nightly builds are currently failing due to our ARM-based jobs.
    These jobs crash immediately when entering the Docker container with a
    exception thrown by Go's language runtime. As we're able to successfully
    builds the Docker images in previous steps, it's unlikely to be a bug in
    Docker itself. Instead, this exception is thrown by gosu(1), which is a
    Go-based utility to drop privileges and run by our entrypoint.
    
    Fix the issue by dropping gosu(1) in favor of sudo(1).

--- azure-pipelines/docker/entrypoint.sh
+++ azure-pipelines/docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 useradd --shell /bin/bash libgit2
-chown -R $(id -u libgit2) /home/libgit2
-exec gosu libgit2 "$@"
+chown --recursive libgit2:libgit2 /home/libgit2
+exec sudo --preserve-env --user=libgit2 "$@"
