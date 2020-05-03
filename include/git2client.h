13aa30e479a662086b09a90b611fb62426199656
Edward Thomson <ethomson@edwardthomson.com>
2020-05-03 19:59:19 +0100

    libgit2client: a client "middleware" library
    Introduce libgit2client, a client "middleware" library.  This is an
    experimental set of utility functions and classes for client software
    that builds on top of libgit2.
    
    This library might contain - for example - code that invokes filters or
    other tools.  This is incredibly useful to share and reuse among
    consumers but should be excluded from libgit2 itself.  Users may,
    understandably, not want code that executes arbitrary other commands in
    libgit2 itself.

---
+++ include/git2client.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) the libgit2 contributors. All rights reserved.
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
+
+#ifndef INCLUDE_git_client_h__
+#define INCLUDE_git_client_h__
+
+#include <git2.h>
+#include "git2client/global.h"
+
+#endif
