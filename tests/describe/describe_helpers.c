ca92646688fae0e44bb82a8a27c804aa6c328c2a
Edward Thomson <ethomson@edwardthomson.com>
2020-05-27 22:08:24 +0100

    describe: user-facing functions write to a userbuf

--- tests/describe/describe_helpers.c
+++ tests/describe/describe_helpers.c
@@ -10,7 +10,7 @@ void assert_describe(
 	git_describe_format_options *fmt_opts)
 {
 	git_object *object;
-	git_buf label = GIT_BUF_INIT;
+	git_userbuf label = GIT_USERBUF_INIT;
 	git_describe_result *result;
 
 	cl_git_pass(git_revparse_single(&object, repo, revparse_spec));
@@ -18,11 +18,11 @@ void assert_describe(
 	cl_git_pass(git_describe_commit(&result, object, opts));
 	cl_git_pass(git_describe_format(&label, result, fmt_opts));
 
-	cl_must_pass(wildmatch(expected_output, git_buf_cstr(&label), 0));
+	cl_must_pass(wildmatch(expected_output, label.ptr, 0));
 
 	git_describe_result_free(result);
 	git_object_free(object);
-	git_buf_dispose(&label);
+	git_userbuf_dispose(&label);
 }
 
 void assert_describe_workdir(
@@ -31,14 +31,14 @@ void assert_describe_workdir(
 	git_describe_options *opts,
 	git_describe_format_options *fmt_opts)
 {
-	git_buf label = GIT_BUF_INIT;
+	git_userbuf label = GIT_USERBUF_INIT;
 	git_describe_result *result;
 
 	cl_git_pass(git_describe_workdir(&result, repo, opts));
 	cl_git_pass(git_describe_format(&label, result, fmt_opts));
 
-	cl_must_pass(wildmatch(expected_output, git_buf_cstr(&label), 0));
+	cl_must_pass(wildmatch(expected_output, label.ptr, 0));
 
 	git_describe_result_free(result);
-	git_buf_dispose(&label);
+	git_userbuf_dispose(&label);
 }
