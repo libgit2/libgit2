634c285a0eb016a2d144b35298dabc350d2e7576
Edward Thomson <ethomson@edwardthomson.com>
2020-07-11 12:35:58 +0100

    Move libgit2 settings into libgit2.c

---
+++ src/libgit2.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) the libgit2 contributors. All rights reserved.
+ *
+ * This file is part of libgit2, distributed under the GNU GPL v2 with
+ * a Linking Exception. For full terms see the included COPYING file.
+ */
+#ifndef INCLUDE_libgit2_h__
+#define INCLUDE_libgit2_h__
+
+extern const char *git_libgit2__user_agent(void);
+extern const char *git_libgit2__ssl_ciphers(void);
+
+#endif
