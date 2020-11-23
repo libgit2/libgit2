dfdda20e1e3cc1b3745512ac6a53ac08f366ca3d
Dhruva Krishnamurthy <dhruvakm@gmail.com>
2020-11-23 13:52:45 -0800

    Use vectored IO for mmap emulation using preadv/pwritev

--- src/unix/posix_unix.c
+++ src/unix/posix_unix.c
@@ -9,16 +9,55 @@
 #include "../posix.h"
 
 #ifndef GIT_WIN32
+
+#ifdef USE_VECTORED_IO
+#include <sys/uio.h>
+
+static int prepare_iovec(uint8_t *data, size_t size, struct iovec *iov,
+		int count) {
+	int cc;
+	size_t chunk;
+	size_t page_size;
+
+	git__page_size(&page_size);
+	count = min((size > page_size) ? (int)(size/page_size) : 1, count);
+	chunk = size / count;
+
+	for (cc = 0; cc < count; ++cc) {
+		iov[cc].iov_base = &data[chunk * cc];
+		iov[cc].iov_len = chunk;
+	}
+
+	/* Last chunk reads the extra bytes beyond a chunk */
+	if (count > 1) {
+		iov[count - 1].iov_len = chunk + (size % chunk);
+	}
+
+	return count;
+}
+#endif
+
 ssize_t p_pread(git_file fd, void *data, size_t size, git_off_t offset)
 {
 	char *b = (char *)data;
+
+#ifdef USE_VECTORED_IO
+	struct iovec iov[8];
+	int iov_count = prepare_iovec((uint8_t *)data, size, iov,
+			sizeof(iov)/sizeof(iov[0]));
+#endif
+
 	if (!git__is_ssizet(size)) {
 		errno = EINVAL;
 		return -1;
 	}
 
 	while (size > 0) {
+#ifdef USE_VECTORED_IO
+		ssize_t r = preadv(fd, iov, iov_count, offset);
+#else
 		ssize_t r = pread(fd, b, size, offset);
+#endif
 		if (r < 0) {
 			if (errno == EINTR || GIT_ISBLOCKED(errno))
 				continue;
@@ -38,13 +77,24 @@ ssize_t p_pread(git_file fd, void *data, size_t size, git_off_t offset)
 ssize_t p_pwrite(git_file fd, const void *data, size_t size, git_off_t offset)
 {
 	char *b = (char *)data;
+
+#ifdef USE_VECTORED_IO
+	struct iovec iov[8];
+	int iov_count = prepare_iovec((uint8_t *)data, size, iov,
+			sizeof(iov)/sizeof(iov[0]));
+#endif
+
 	if (!git__is_ssizet(size)) {
 		errno = EINVAL;
 		return -1;
 	}
 
 	while (size > 0) {
+#ifdef USE_VECTORED_IO
+		ssize_t r = pwritev(fd, iov, iov_count, offset);
+#else
 		ssize_t r = pwrite(fd, b, size, offset);
+#endif
 		if (r < 0) {
 			if (errno == EINTR || GIT_ISBLOCKED(errno))
 				continue;
