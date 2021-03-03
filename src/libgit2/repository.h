1cea2299819b539b341ccf05d3809829be642841
Jochen Hunz <j.hunz@anchorpoint.app>
2021-03-03 15:10:28 +0100

    add core.sparseCheckout to configmap cache
    Signed-off-by: Jochen Hunz <j.hunz@anchorpoint.app>

--- src/libgit2/repository.h
+++ src/libgit2/repository.h
@@ -53,6 +53,7 @@ typedef enum {
 	GIT_CONFIGMAP_PROTECTNTFS,      /* core.protectNTFS */
 	GIT_CONFIGMAP_FSYNCOBJECTFILES, /* core.fsyncObjectFiles */
 	GIT_CONFIGMAP_LONGPATHS,        /* core.longpaths */
+	GIT_CONFIGMAP_SPARSECHECKOUT,   /* core.sparseCheckout */
 	GIT_CONFIGMAP_CACHE_MAX
 } git_configmap_item;
 
@@ -119,7 +120,9 @@ typedef enum {
 	/* core.fsyncObjectFiles */
 	GIT_FSYNCOBJECTFILES_DEFAULT = GIT_CONFIGMAP_FALSE,
 	/* core.longpaths */
-	GIT_LONGPATHS_DEFAULT = GIT_CONFIGMAP_FALSE
+	GIT_LONGPATHS_DEFAULT = GIT_CONFIGMAP_FALSE,
+	/* core.sparseCheckout */
+	GIT_SPARSECHECKOUT_DEFAULT = GIT_CONFIGMAP_FALSE,
 } git_configmap_value;
 
 /* internal repository init flags */
