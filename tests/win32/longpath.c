33667c14eb000b56edb343b90cbdafe7a1f88e56
Edward Thomson <ethomson@edwardthomson.com>
2021-05-05 23:59:05 +0100

    win32: add file and get status on a long path

--- tests/win32/longpath.c
+++ tests/win32/longpath.c
@@ -8,6 +8,8 @@
 
 static git_buf path = GIT_BUF_INIT;
 
+#define LONG_FILENAME "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt"
+
 void test_win32_longpath__initialize(void)
 {
 #ifdef GIT_WIN32
@@ -52,11 +54,39 @@ void test_win32_longpath__workdir_path_validated(void)
 	cl_git_pass(git_repository_workdir_path(&out, repo, "a.txt"));
 
 	/* even if the repo path is a drive letter, this is too long */
-	cl_git_fail(git_repository_workdir_path(&out, repo, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt"));
+	cl_git_fail(git_repository_workdir_path(&out, repo, LONG_FILENAME));
 	cl_assert(git__prefixcmp(git_error_last()->message, "path too long") == 0);
 
 	cl_repo_set_bool(repo, "core.longpaths", true);
 	cl_git_pass(git_repository_workdir_path(&out, repo, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt"));
+	cl_git_pass(git_repository_workdir_path(&out, repo, LONG_FILENAME));
+	git_buf_dispose(&out);
+#endif
+}
+
+void test_win32_longpath__status_and_add(void)
+{
+#ifdef GIT_WIN32
+	git_repository *repo = cl_git_sandbox_init("testrepo");
+	git_index *index;
+	git_buf out = GIT_BUF_INIT;
+	unsigned int status_flags;
+
+	cl_repo_set_bool(repo, "core.longpaths", true);
+	cl_git_pass(git_repository_workdir_path(&out, repo, LONG_FILENAME));
+
+	cl_git_rewritefile(out.ptr, "This is a long path.\r\n");
+
+	cl_git_pass(git_status_file(&status_flags, repo, LONG_FILENAME));
+	cl_assert_equal_i(GIT_STATUS_WT_NEW, status_flags);
+
+	cl_git_pass(git_repository_index(&index, repo));
+	cl_git_pass(git_index_add_bypath(index, LONG_FILENAME));
+
+	cl_git_pass(git_status_file(&status_flags, repo, LONG_FILENAME));
+	cl_assert_equal_i(GIT_STATUS_INDEX_NEW, status_flags);
+
+	git_index_free(index);
 	git_buf_dispose(&out);
 #endif
 }
