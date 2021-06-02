8b79a3f9baed9077a9163314bb3525166c792323
punkymaniac <punkymaniac@protonmail.ch>
2021-06-02 14:46:12 +0200

    Remove bad space in documentation

--- include/git2/indexer.h
+++ include/git2/indexer.h
@@ -51,7 +51,7 @@ typedef struct git_indexer_progress {
  * Type for progress callbacks during indexing.  Return a value less
  * than zero to cancel the indexing or download.
  *
- * @param stats Structure containing information about the state of the tran    sfer
+ * @param stats Structure containing information about the state of the transfer
  * @param payload Payload provided by caller
  */
 typedef int GIT_CALLBACK(git_indexer_progress_cb)(const git_indexer_progress *stats, void *payload);
