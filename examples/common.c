2e30cf6e4c64250b0e5e77cd1fea1a46374848c8
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-06-16 11:05:21 +0200

    Expansion for "~" to $HOME on iOS

--- examples/common.c
+++ examples/common.c
@@ -269,10 +269,10 @@ int cred_acquire_cb(git_credential **out,
 		// iOS: we only expand ~/ 
 		home = getenv("HOME");
 		if ((strncmp(privkey, "~/", 2) == 0) && (home != NULL)) {
-			n = snprintf(NULL, 0, "%s/.ssh%s", home, privkey + 1);
+			n = snprintf(NULL, 0, "%s/%s", home, privkey + 1);
 			newPrivkey = malloc(n + 1);
 			if (newPrivkey != NULL) {
-				snprintf(newPrivkey, n + 1, "%s/.ssh%s", home, privkey + 1);
+				snprintf(newPrivkey, n + 1, "%s/%s", home, privkey + 1);
 				free(privkey);
 				privkey = newPrivkey;
 			}
