aa5c288acfae5506c5b84117b4115e684a60a100
Henry Heino <hheino@uw.edu>
2021-06-22 11:45:16 -0700

    commit: Refuse to create empty commits

--- examples/commit.c
+++ examples/commit.c
@@ -19,11 +19,14 @@ typedef struct {
 	int amend_head;
 } commit_options;
 
-/** Don't write an encoding header. Assume UTF-8. */
+/** Don't write an encoding header. NULL causes libgit2 to assume UTF-8. */
 #define MESSAGE_ENCODING NULL
 
 /// Returns 0 on success.
 static int parse_options(commit_options *out, int argc, char **argv);
+
+/// Returns 1 if there are staged changes, 0 if not (or if an error occurred).
+static int are_staged_changes(int *error, git_repository *repo);
 
 /**
  * This example demonstrates the libgit2 commit APIs to roughly
@@ -81,13 +84,41 @@ int lg2_commit(git_repository *repo, int argc, char **argv)
 		else printf("ERROR %d: no detailed info\n", error);
 
 		goto cleanup;
-	} else
+	}
 
 	check_lg2(git_repository_index(&index, repo), "Could not open repository index", NULL);
 	check_lg2(git_index_write_tree(&tree_oid, index), "Could not write tree", NULL);
 	check_lg2(git_index_write(index), "Could not write index", NULL);
 
 	check_lg2(git_tree_lookup(&tree, repo, &tree_oid), "Error looking up tree", NULL);
+
+	/**
+	 * Don't create empty commits!
+	 */
+	if (!are_staged_changes(&error, repo)) {
+		if (error) {
+			fprintf(stderr, "Error determining whether there are staged changes!\n");
+			goto cleanup;
+		}
+
+		// If we're not amending, it's an error.
+		if (!opts.amend_head) {
+			fprintf(stderr, "Error: No staged changes (nothing would be in the commit!)."
+					" Refusing to commit.\n");
+			fprintf(stderr,
+				"Try running:\n"
+				" *	lg2 status\n"
+				"	    to see which changes are staged (ready to commit) and which"
+				" are unstaged.\n"
+				" *	lg2 add path/to/changed/file\n"
+				"	    to stage a file.\n"
+				"\n");
+			error = -1;
+			goto cleanup;
+		} else {
+			fprintf(stderr, "Note: There were no staged changes.\n");
+		}
+	}
 
 	error = git_signature_default(&signature, repo);
 	if (error) {
@@ -131,6 +162,29 @@ cleanup:
 	git_tree_free(tree);
 
 	return error;
+}
+
+static int are_staged_changes(int *error, git_repository *repo)
+{
+	int result = 0;
+	git_status_options status_opts;
+	git_status_list *status_list = NULL;
+
+	*error = git_status_options_init(&status_opts, GIT_STATUS_OPTIONS_VERSION);
+	if (*error) goto cleanup;
+
+	// We're only interested in changes that are staged -- those that
+	// are indexed.
+	status_opts.show = GIT_STATUS_SHOW_INDEX_ONLY;
+
+	*error = git_status_list_new(&status_list, repo, &status_opts);
+	if (*error) goto cleanup;
+
+	result = git_status_list_entrycount(status_list);
+
+cleanup:
+	git_status_list_free(status_list);
+	return result;
 }
 
 static int parse_options(commit_options *out, int argc, char **argv)
