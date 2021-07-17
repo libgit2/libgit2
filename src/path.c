e81a56c39bfe69ea4f45b07b9686fe54411e2bf2
Calvin Buckley <calvin@cmpct.info>
2021-07-17 14:25:20 -0300

    Enum field isn't supported pre-Vista

--- src/path.c
+++ src/path.c
@@ -1223,7 +1223,11 @@ int git_path_diriter_init(
 
 	diriter->handle = FindFirstFileExW(
 		path_filter,
+#if _WIN32_WINNT >= 0x0600
 		is_win7_or_later ? FindExInfoBasic : FindExInfoStandard,
+#else
+		FindExInfoStandard,
+#endif
 		&diriter->current,
 		FindExSearchNameMatch,
 		NULL,
