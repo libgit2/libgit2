e265eb2f51a13d0a1172330075bdd1c69aed5e8c
Edward Thomson <ethomson@edwardthomson.com>
2021-08-24 13:58:24 -0400

    tests: show errors when libgit2_init fails

--- tests/main.c
+++ tests/main.c
@@ -18,7 +18,9 @@ int main(int argc, char *argv[])
 
 	res = git_libgit2_init();
 	if (res < 0) {
-		fprintf(stderr, "failed to init libgit2");
+		const git_error *err = git_error_last();
+		const char *msg = err ? err->message : "unknown failure";
+		fprintf(stderr, "failed to init libgit2: %s\n", msg);
 		return res;
 	}
 
