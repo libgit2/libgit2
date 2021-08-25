cdb9f3903ef650a5289ea944770ba38d688e5899
Zachary Michaels <mikezackles@gmail.com>
2021-08-24 17:37:15 -0700

    mbedTLS: Fix setting certificate directory
    fixes #6003

--- src/streams/mbedtls.h
+++ src/streams/mbedtls.h
@@ -14,7 +14,7 @@
 extern int git_mbedtls_stream_global_init(void);
 
 #ifdef GIT_MBEDTLS
-extern int git_mbedtls__set_cert_location(const char *path, int is_dir);
+extern int git_mbedtls__set_cert_location(const char *file, const char *path);
 
 extern int git_mbedtls_stream_new(git_stream **out, const char *host, const char *port);
 extern int git_mbedtls_stream_wrap(git_stream **out, git_stream *in, const char *host);
