wcribb7/wcribbz

str: introduce `git_str` for internal, `git_buf` is external
libgit2 has two distinct requirements that were previously solved by
`git_buf`.  We require:

1. A general purpose string class that provides a number of utility APIs
   for manipulating data (eg, concatenating, truncating, etc).
2. A structure that we can use to return strings to callers that they
   can take ownership of.

By using a single class (`git_buf`) for both of these purposes, we have
confused the API to the point that refactorings are difficult and
reasoning about correctness is also difficult.

Move the utility class `git_buf` to be called `git_str`: this represents
its general purpose, as an internal string buffer class.  The name also
is an homage to Junio Hamano ("gitstr").

The public API remains `git_buf`, and has a much smaller footprint.  It
is generally only used as an "out" param with strict requirements that
follow the documentation.  (Exceptions exist for some legacy APIs to
avoid breaking callers unnecessarily.)

Utility functions exist to convert a user-specified `git_buf` to a
`git_str` so that we can call internal functions, then converting it
back again.

src/diff.c
@@ -8,6 +8,7 @@
 #include "diff.h"
 
 #include "common.h"
+#include "buf.h"
 #include "patch.h"
 #include "email.h"
 #include "commit.h"
@@ -162,6 +163,7 @@ int git_diff_format_email(
 	const git_diff_format_email_options *opts)
 {
 	git_email_create_options email_create_opts = GIT_EMAIL_CREATE_OPTIONS_INIT;
+	git_str email = GIT_STR_INIT;
 	int error;
 
 	GIT_ASSERT_ARG(out);
@@ -172,14 +174,29 @@ int git_diff_format_email(
 		GIT_DIFF_FORMAT_EMAIL_OPTIONS_VERSION,
 		"git_format_email_options");
 
+	/* This is a `git_buf` special case; subsequent calls append. */
+	email.ptr = out->ptr;
+	email.asize = out->reserved;
+	email.size = out->size;
+
+	out->ptr = git_str__initstr;
+	out->reserved = 0;
+	out->size = 0;
+
 	if ((opts->flags & GIT_DIFF_FORMAT_EMAIL_EXCLUDE_SUBJECT_PATCH_MARKER) != 0)
 		email_create_opts.subject_prefix = "";
 
-
-	error = git_email__append_from_diff(out, diff, opts->patch_no,
+	error = git_email__append_from_diff(&email, diff, opts->patch_no,
 		opts->total_patches, opts->id, opts->summary, opts->body,
 		opts->author, &email_create_opts);
 
+	if (error < 0)
+		goto done;
+
+	error = git_buf_fromstr(out, &email);
+
+done:
+	git_str_dispose(&email);
 	return error;
 }
 
@@ -282,7 +299,7 @@ static int flush_hunk(git_oid *result, git_hash_ctx *ctx)
 	return 0;
 }
 
-static void strip_spaces(git_buf *buf)
+static void strip_spaces(git_str *buf)
 {
 	char *src = buf->ptr, *dst = buf->ptr;
 	char c;
@@ -295,7 +312,7 @@ static void strip_spaces(git_buf *buf)
 		}
 	}
 
-	git_buf_truncate(buf, len);
+	git_str_truncate(buf, len);
 }
 
 static int diff_patchid_print_callback_to_buf(
@@ -305,7 +322,7 @@ static int diff_patchid_print_callback_to_buf(
 	void *payload)
 {
 	struct patch_id_args *args = (struct patch_id_args *) payload;
-	git_buf buf = GIT_BUF_INIT;
+	git_str buf = GIT_BUF_INIT;
 	int error = 0;
 
 	if (line->origin == GIT_DIFF_LINE_CONTEXT_EOFNL ||
@@ -331,7 +348,7 @@ static int diff_patchid_print_callback_to_buf(
 		args->first_file = 0;
 
 out:
-	git_buf_dispose(&buf);
+	git_str_dispose(&buf);
 	return error;
 }
 

tests/repo/hashfile.c
@@ -1,5 +1,4 @@
 #include "clar_libgit2.h"
-#include "buffer.h"
 
 static git_repository *_repo;
 
@@ -18,14 +17,14 @@ void test_repo_hashfile__cleanup(void)
 void test_repo_hashfile__simple(void)
 {
 	git_oid a, b;
-	git_buf full = GIT_BUF_INIT;
+	git_str full = GIT_STR_INIT;
 
 	/* hash with repo relative path */
 	cl_git_pass(git_odb_hashfile(&a, "status/current_file", GIT_OBJECT_BLOB));
 	cl_git_pass(git_repository_hashfile(&b, _repo, "current_file", GIT_OBJECT_BLOB, NULL));
 	cl_assert_equal_oid(&a, &b);
 
-	cl_git_pass(git_buf_joinpath(&full, git_repository_workdir(_repo), "current_file"));
+	cl_git_pass(git_str_joinpath(&full, git_repository_workdir(_repo), "current_file"));
 
 	/* hash with full path */
 	cl_git_pass(git_odb_hashfile(&a, full.ptr, GIT_OBJECT_BLOB));
@@ -36,20 +35,20 @@ void test_repo_hashfile__simple(void)
 	cl_git_fail(git_odb_hashfile(&a, full.ptr, GIT_OBJECT_ANY));
 	cl_git_fail(git_repository_hashfile(&b, _repo, full.ptr, GIT_OBJECT_OFS_DELTA, NULL));
 
-	git_buf_dispose(&full);
+	git_str_dispose(&full);
 }
 
 void test_repo_hashfile__filtered_in_workdir(void)
 {
-	git_buf root = GIT_BUF_INIT, txt = GIT_BUF_INIT, bin = GIT_BUF_INIT;
+	git_str root = GIT_BUF_INIT, txt = GIT_BUF_INIT, bin = GIT_BUF_INIT;
 	char cwd[GIT_PATH_MAX];
 	git_oid a, b;
 
 	cl_must_pass(p_getcwd(cwd, GIT_PATH_MAX));
 	cl_must_pass(p_mkdir("absolute", 0777));
-	cl_git_pass(git_buf_joinpath(&root, cwd, "status"));
-	cl_git_pass(git_buf_joinpath(&txt, root.ptr, "testfile.txt"));
-	cl_git_pass(git_buf_joinpath(&bin, root.ptr, "testfile.bin"));
+	cl_git_pass(git_str_joinpath(&root, cwd, "status"));
+	cl_git_pass(git_str_joinpath(&txt, root.ptr, "testfile.txt"));
+	cl_git_pass(git_str_joinpath(&bin, root.ptr, "testfile.bin"));
 
 	cl_repo_set_bool(_repo, "core.autocrlf", true);
 
@@ -120,22 +119,22 @@ void test_repo_hashfile__filtered_in_workdir(void)
 	cl_git_fail(git_odb_hashfile(&a, "status/testfile.txt", 0));
 	cl_git_fail(git_repository_hashfile(&b, _repo, "testfile.txt", GIT_OBJECT_ANY, NULL));
 
-	git_buf_dispose(&txt);
-	git_buf_dispose(&bin);
-	git_buf_dispose(&root);
+	git_str_dispose(&txt);
+	git_str_dispose(&bin);
+	git_str_dispose(&root);
 }
 
 void test_repo_hashfile__filtered_outside_workdir(void)
 {
-	git_buf root = GIT_BUF_INIT, txt = GIT_BUF_INIT, bin = GIT_BUF_INIT;
+	git_str root = GIT_BUF_INIT, txt = GIT_BUF_INIT, bin = GIT_BUF_INIT;
 	char cwd[GIT_PATH_MAX];
 	git_oid a, b;
 
 	cl_must_pass(p_getcwd(cwd, GIT_PATH_MAX));
 	cl_must_pass(p_mkdir("absolute", 0777));
-	cl_git_pass(git_buf_joinpath(&root, cwd, "absolute"));
-	cl_git_pass(git_buf_joinpath(&txt, root.ptr, "testfile.txt"));
-	cl_git_pass(git_buf_joinpath(&bin, root.ptr, "testfile.bin"));
+	cl_git_pass(git_str_joinpath(&root, cwd, "absolute"));
+	cl_git_pass(git_str_joinpath(&txt, root.ptr, "testfile.txt"));
+	cl_git_pass(git_str_joinpath(&bin, root.ptr, "testfile.bin"));
 
 	cl_repo_set_bool(_repo, "core.autocrlf", true);
 	cl_git_append2file("status/.gitattributes", "*.txt text\n*.bin binary\n\n");
@@ -166,7 +165,7 @@ void test_repo_hashfile__filtered_outside_workdir(void)
 	cl_git_pass(git_repository_hashfile(&b, _repo, bin.ptr, GIT_OBJECT_BLOB, NULL));
 	cl_assert_equal_oid(&a, &b);
 
-	git_buf_dispose(&txt);
-	git_buf_dispose(&bin);
-	git_buf_dispose(&root);
+	git_str_dispose(&txt);
+	git_str_dispose(&bin);
+	git_str_dispose(&root);
 }

libgit2/libgit2

src/diff.c
@@ -8,6 +8,7 @@
 #include "diff.h"
 
 #include "common.h"
+#include "buf.h"
 #include "patch.h"
 #include "email.h"
 #include "commit.h"
@@ -162,6 +163,7 @@ int git_diff_format_email(
 	const git_diff_format_email_options *opts)
 {
 	git_email_create_options email_create_opts = GIT_EMAIL_CREATE_OPTIONS_INIT;
+	git_str email = GIT_STR_INIT;
 	int error;
 
 	GIT_ASSERT_ARG(out);
@@ -172,14 +174,29 @@ int git_diff_format_email(
 		GIT_DIFF_FORMAT_EMAIL_OPTIONS_VERSION,
 		"git_format_email_options");
 
+	/* This is a `git_buf` special case; subsequent calls append. */
+	email.ptr = out->ptr;
+	email.asize = out->reserved;
+	email.size = out->size;
+
+	out->ptr = git_str__initstr;
+	out->reserved = 0;
+	out->size = 0;
+
 	if ((opts->flags & GIT_DIFF_FORMAT_EMAIL_EXCLUDE_SUBJECT_PATCH_MARKER) != 0)
 		email_create_opts.subject_prefix = "";
 
-
-	error = git_email__append_from_diff(out, diff, opts->patch_no,
+	error = git_email__append_from_diff(&email, diff, opts->patch_no,
 		opts->total_patches, opts->id, opts->summary, opts->body,
 		opts->author, &email_create_opts);
 
+	if (error < 0)
+		goto done;
+
+	error = git_buf_fromstr(out, &email);
+
+done:
+	git_str_dispose(&email);
 	return error;
 }
 
@@ -282,7 +299,7 @@ static int flush_hunk(git_oid *result, git_hash_ctx *ctx)
 	return 0;
 }
 
-static void strip_spaces(git_buf *buf)
+static void strip_spaces(git_str *buf)
 {
 	char *src = buf->ptr, *dst = buf->ptr;
 	char c;
@@ -295,7 +312,7 @@ static void strip_spaces(git_buf *buf)
 		}
 	}
 
-	git_buf_truncate(buf, len);
+	git_str_truncate(buf, len);
 }
 
 static int diff_patchid_print_callback_to_buf(
@@ -305,7 +322,7 @@ static int diff_patchid_print_callback_to_buf(
 	void *payload)
 {
 	struct patch_id_args *args = (struct patch_id_args *) payload;
-	git_buf buf = GIT_BUF_INIT;
+	git_str buf = GIT_STR_INIT;
 	int error = 0;
 
 	if (line->origin == GIT_DIFF_LINE_CONTEXT_EOFNL ||
@@ -331,7 +348,7 @@ static int diff_patchid_print_callback_to_buf(
 		args->first_file = 0;
 
 out:
-	git_buf_dispose(&buf);
+	git_str_dispose(&buf);
 	return error;
 }
 

tests/repo/hashfile.c
@@ -1,5 +1,4 @@
 #include "clar_libgit2.h"
-#include "buffer.h"
 
 static git_repository *_repo;
 
@@ -18,14 +17,14 @@ void test_repo_hashfile__cleanup(void)
 void test_repo_hashfile__simple(void)
 {
 	git_oid a, b;
-	git_buf full = GIT_BUF_INIT;
+	git_str full = GIT_STR_INIT;
 
 	/* hash with repo relative path */
 	cl_git_pass(git_odb_hashfile(&a, "status/current_file", GIT_OBJECT_BLOB));
 	cl_git_pass(git_repository_hashfile(&b, _repo, "current_file", GIT_OBJECT_BLOB, NULL));
 	cl_assert_equal_oid(&a, &b);
 
-	cl_git_pass(git_buf_joinpath(&full, git_repository_workdir(_repo), "current_file"));
+	cl_git_pass(git_str_joinpath(&full, git_repository_workdir(_repo), "current_file"));
 
 	/* hash with full path */
 	cl_git_pass(git_odb_hashfile(&a, full.ptr, GIT_OBJECT_BLOB));
@@ -36,20 +35,20 @@ void test_repo_hashfile__simple(void)
 	cl_git_fail(git_odb_hashfile(&a, full.ptr, GIT_OBJECT_ANY));
 	cl_git_fail(git_repository_hashfile(&b, _repo, full.ptr, GIT_OBJECT_OFS_DELTA, NULL));
 
-	git_buf_dispose(&full);
+	git_str_dispose(&full);
 }
 
 void test_repo_hashfile__filtered_in_workdir(void)
 {
-	git_buf root = GIT_BUF_INIT, txt = GIT_BUF_INIT, bin = GIT_BUF_INIT;
+	git_str root = GIT_STR_INIT, txt = GIT_STR_INIT, bin = GIT_STR_INIT;
 	char cwd[GIT_PATH_MAX];
 	git_oid a, b;
 
 	cl_must_pass(p_getcwd(cwd, GIT_PATH_MAX));
 	cl_must_pass(p_mkdir("absolute", 0777));
-	cl_git_pass(git_buf_joinpath(&root, cwd, "status"));
-	cl_git_pass(git_buf_joinpath(&txt, root.ptr, "testfile.txt"));
-	cl_git_pass(git_buf_joinpath(&bin, root.ptr, "testfile.bin"));
+	cl_git_pass(git_str_joinpath(&root, cwd, "status"));
+	cl_git_pass(git_str_joinpath(&txt, root.ptr, "testfile.txt"));
+	cl_git_pass(git_str_joinpath(&bin, root.ptr, "testfile.bin"));
 
 	cl_repo_set_bool(_repo, "core.autocrlf", true);
 
@@ -120,22 +119,22 @@ void test_repo_hashfile__filtered_in_workdir(void)
 	cl_git_fail(git_odb_hashfile(&a, "status/testfile.txt", 0));
 	cl_git_fail(git_repository_hashfile(&b, _repo, "testfile.txt", GIT_OBJECT_ANY, NULL));
 
-	git_buf_dispose(&txt);
-	git_buf_dispose(&bin);
-	git_buf_dispose(&root);
+	git_str_dispose(&txt);
+	git_str_dispose(&bin);
+	git_str_dispose(&root);
 }
 
 void test_repo_hashfile__filtered_outside_workdir(void)
 {
-	git_buf root = GIT_BUF_INIT, txt = GIT_BUF_INIT, bin = GIT_BUF_INIT;
+	git_str root = GIT_STR_INIT, txt = GIT_STR_INIT, bin = GIT_STR_INIT;
 	char cwd[GIT_PATH_MAX];
 	git_oid a, b;
 
 	cl_must_pass(p_getcwd(cwd, GIT_PATH_MAX));
 	cl_must_pass(p_mkdir("absolute", 0777));
-	cl_git_pass(git_buf_joinpath(&root, cwd, "absolute"));
-	cl_git_pass(git_buf_joinpath(&txt, root.ptr, "testfile.txt"));
-	cl_git_pass(git_buf_joinpath(&bin, root.ptr, "testfile.bin"));
+	cl_git_pass(git_str_joinpath(&root, cwd, "absolute"));
+	cl_git_pass(git_str_joinpath(&txt, root.ptr, "testfile.txt"));
+	cl_git_pass(git_str_joinpath(&bin, root.ptr, "testfile.bin"));
 
 	cl_repo_set_bool(_repo, "core.autocrlf", true);
 	cl_git_append2file("status/.gitattributes", "*.txt text\n*.bin binary\n\n");
@@ -166,7 +165,7 @@ void test_repo_hashfile__filtered_outside_workdir(void)
 	cl_git_pass(git_repository_hashfile(&b, _repo, bin.ptr, GIT_OBJECT_BLOB, NULL));
 	cl_assert_equal_oid(&a, &b);
 
-	git_buf_dispose(&txt);
-	git_buf_dispose(&bin);
-	git_buf_dispose(&root);
+	git_str_dispose(&txt);
+	git_str_dispose(&bin);
+	git_str_dispose(&root);
 }