b877122750aa0435bab701825378754783d09485
Edward Thomson <ethomson@edwardthomson.com>
2021-11-28 10:32:03 -0500

    cli: add `cat-file` command
    Introduce a simple command that emulates `git cat-file`.

--- src/cli/cmd.h
+++ src/cli/cmd.h
@@ -25,6 +25,7 @@ extern const cli_cmd_spec cli_cmds[];
 extern const cli_cmd_spec *cli_cmd_spec_byname(const char *name);
 
 /* Commands */
+extern int cmd_cat_file(int argc, char **argv);
 extern int cmd_help(int argc, char **argv);
 
 #endif /* CLI_cmd_h__ */
