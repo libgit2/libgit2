6aa3603ae44b077c8dc0a506e3cdf989cd3a473e
Peter Pettersson <boretrk@hotmail.com>
2021-12-31 01:50:45 +0100

    ntmlclient: don't declare dummy HMAC_CTX_cleanup when building with libressl

--- deps/ntlmclient/crypt_openssl.c
+++ deps/ntlmclient/crypt_openssl.c
@@ -44,7 +44,7 @@ static inline void HMAC_CTX_free(HMAC_CTX *ctx)
 
 #endif
 
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L || defined(CRYPT_OPENSSL_DYNAMIC)
+#if (OPENSSL_VERSION_NUMBER >= 0x10100000L && !LIBRESSL_VERSION_NUMBER) || defined(CRYPT_OPENSSL_DYNAMIC)
 
 static inline void HMAC_CTX_cleanup(HMAC_CTX *ctx)
 {
