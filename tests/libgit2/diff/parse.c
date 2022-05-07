4ad794a02b403f3c420f20f1245082f0d12a3cc0
Cavaquinho <92610329+cavaquinho@users.noreply.github.com>
2022-05-06 18:59:25 -0700

    Add tests that demonstrate issue #6294

--- tests/libgit2/diff/parse.c
+++ tests/libgit2/diff/parse.c
@@ -413,6 +413,24 @@ void test_diff_parse__lineinfo(void)
 }
 
 
+void test_diff_parse__new_file_no_spaces(void)
+{
+	const char *content = PATCH_ORIGINAL_NEW_FILE_NO_SPACES;
+	git_patch *patch;
+	git_diff *diff;
+
+	cl_git_pass(git_diff_from_buffer(&diff, content, strlen(content)));
+	cl_git_pass(git_patch_from_diff((git_patch **) &patch, diff, 0));
+
+	cl_assert_equal_s(patch->diff_opts.old_prefix, "a/");
+	cl_assert_equal_s(patch->delta->old_file.path, "no_space.txt");
+	cl_assert_equal_s(patch->diff_opts.new_prefix, "b/");
+	cl_assert_equal_s(patch->delta->new_file.path, "no_space.txt");
+
+	git_patch_free(patch);
+	git_diff_free(diff);
+}
+
 void test_diff_parse__new_file_with_space(void)
 {
 	const char *content = PATCH_ORIGINAL_NEW_FILE_WITH_SPACE;
@@ -422,8 +440,8 @@ void test_diff_parse__new_file_with_space(void)
 	cl_git_pass(git_diff_from_buffer(&diff, content, strlen(content)));
 	cl_git_pass(git_patch_from_diff((git_patch **) &patch, diff, 0));
 
-	cl_assert_equal_p(patch->diff_opts.old_prefix, NULL);
-	cl_assert_equal_p(patch->delta->old_file.path, NULL);
+	cl_assert_equal_s(patch->diff_opts.old_prefix, "a/");
+	cl_assert_equal_s(patch->delta->old_file.path, "sp ace.txt");
 	cl_assert_equal_s(patch->diff_opts.new_prefix, "b/");
 	cl_assert_equal_s(patch->delta->new_file.path, "sp ace.txt");
 
