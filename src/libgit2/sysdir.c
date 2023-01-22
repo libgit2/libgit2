bce17198f561ed85c0788adc8f0a3259a18a545d
Edward Thomson <ethomson@edwardthomson.com>
2023-01-22 15:23:10 +0000

    sysdir: provide actual home directory
    Provide a mechanism to look up the user's home directory, using the same
    mechanism that we use for locating the global configuration path (a
    fancy name for saying "the home directory").
    
    SSH known hosts lookups now use this, instead of simply looking at the
    HOME environment variable, to support Windows-style home directory
    lookups in `HOME`, `HOMEPATH`, or `USERPROFILE`.

--- src/libgit2/sysdir.c
+++ src/libgit2/sysdir.c
@@ -321,7 +321,7 @@ out:
 }
 #endif
 
-static int git_sysdir_guess_global_dirs(git_str *out)
+static int git_sysdir_guess_home_dirs(git_str *out)
 {
 #ifdef GIT_WIN32
 	static const wchar_t *global_tmpls[4] = {
@@ -365,6 +365,11 @@ static int git_sysdir_guess_global_dirs(git_str *out)
 
 	return error;
 #endif
+}
+
+static int git_sysdir_guess_global_dirs(git_str *out)
+{
+	return git_sysdir_guess_home_dirs(out);
 }
 
 static int git_sysdir_guess_xdg_dirs(git_str *out)
@@ -434,6 +439,7 @@ static struct git_sysdir__dir git_sysdir__dirs[] = {
 	{ GIT_STR_INIT, git_sysdir_guess_xdg_dirs },
 	{ GIT_STR_INIT, git_sysdir_guess_programdata_dirs },
 	{ GIT_STR_INIT, git_sysdir_guess_template_dirs },
+	{ GIT_STR_INIT, git_sysdir_guess_home_dirs }
 };
 
 static void git_sysdir_global_shutdown(void)
@@ -613,11 +619,29 @@ int git_sysdir_find_template_dir(git_str *path)
 		path, NULL, GIT_SYSDIR_TEMPLATE, "template");
 }
 
+int git_sysdir_find_homedir(git_str *path)
+{
+	return git_sysdir_find_in_dirlist(
+		path, NULL, GIT_SYSDIR_HOME, "home directory");
+}
+
 int git_sysdir_expand_global_file(git_str *path, const char *filename)
 {
 	int error;
 
 	if ((error = git_sysdir_find_global_file(path, NULL)) == 0) {
+		if (filename)
+			error = git_str_joinpath(path, path->ptr, filename);
+	}
+
+	return error;
+}
+
+int git_sysdir_expand_homedir_file(git_str *path, const char *filename)
+{
+	int error;
+
+	if ((error = git_sysdir_find_homedir(path)) == 0) {
 		if (filename)
 			error = git_str_joinpath(path, path->ptr, filename);
 	}
