9dd1bfe81c3aba6ce35ed85bf62fa75d9c61e1a6
Edward Thomson <ethomson@edwardthomson.com>
2023-05-14 14:07:59 +0100

    alloc: simplify pluggable allocators
    Remove the number of functions that custom allocator users need to
    provide; nobody should need to implement `substrdup`. Keep it to the
    basics that are actually _needed_ for allocation (malloc, realloc,
    free) and reimplement the rest ourselves.
    
    In addition, move the failure check and error setting _out_ of the
    custom allocators and into a wrapper so that users don't need to deal
    with this. This also allows us to call our allocator (without the
    wrapper) early so that it does not try to set an error on failure, which
    may be important for bootstrapping.

--- src/util/allocators/failalloc.c
+++ src/util/allocators/failalloc.c
@@ -16,70 +16,10 @@ void *git_failalloc_malloc(size_t len, const char *file, int line)
 	return NULL;
 }
 
-void *git_failalloc_calloc(size_t nelem, size_t elsize, const char *file, int line)
-{
-	GIT_UNUSED(nelem);
-	GIT_UNUSED(elsize);
-	GIT_UNUSED(file);
-	GIT_UNUSED(line);
-
-	return NULL;
-}
-
-char *git_failalloc_strdup(const char *str, const char *file, int line)
-{
-	GIT_UNUSED(str);
-	GIT_UNUSED(file);
-	GIT_UNUSED(line);
-
-	return NULL;
-}
-
-char *git_failalloc_strndup(const char *str, size_t n, const char *file, int line)
-{
-	GIT_UNUSED(str);
-	GIT_UNUSED(n);
-	GIT_UNUSED(file);
-	GIT_UNUSED(line);
-
-	return NULL;
-}
-
-char *git_failalloc_substrdup(const char *start, size_t n, const char *file, int line)
-{
-	GIT_UNUSED(start);
-	GIT_UNUSED(n);
-	GIT_UNUSED(file);
-	GIT_UNUSED(line);
-
-	return NULL;
-}
-
 void *git_failalloc_realloc(void *ptr, size_t size, const char *file, int line)
 {
 	GIT_UNUSED(ptr);
 	GIT_UNUSED(size);
-	GIT_UNUSED(file);
-	GIT_UNUSED(line);
-
-	return NULL;
-}
-
-void *git_failalloc_reallocarray(void *ptr, size_t nelem, size_t elsize, const char *file, int line)
-{
-	GIT_UNUSED(ptr);
-	GIT_UNUSED(nelem);
-	GIT_UNUSED(elsize);
-	GIT_UNUSED(file);
-	GIT_UNUSED(line);
-
-	return NULL;
-}
-
-void *git_failalloc_mallocarray(size_t nelem, size_t elsize, const char *file, int line)
-{
-	GIT_UNUSED(nelem);
-	GIT_UNUSED(elsize);
 	GIT_UNUSED(file);
 	GIT_UNUSED(line);
 
