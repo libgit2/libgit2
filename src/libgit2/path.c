c9373e3cfa477c0e53dcd151943b4b7f92ddb22d
Havard Eidnes <he@NetBSD.org>
2023-05-25 15:42:17 +0000

    Ensure well-defined value range for args to <ctype.h> functions.
    Found while building on NetBSD's pkgsrc, which defaults to building
    with -Wchar-subscripts.  To quote the NetBSD ctype(3) man page:
    
    ```
         The argument of these functions is of type int, but only a very
         restricted subset of values are actually valid.  The argument must either
         be the value of the macro EOF (which has a negative value), or must be a
         non-negative value within the range representable as unsigned char.
         Passing invalid values leads to undefined behavior.
    ```
    
    Hence, nearly half of the `signed char` values result in undefined
    behaviour, which cannot be good.
    
    With these changes applied, libgit2 builds without warning on pkgsrc.

--- src/libgit2/path.c
+++ src/libgit2/path.c
@@ -202,7 +202,7 @@ GIT_INLINE(size_t) common_prefix_icase(const char *str, size_t len, const char *
 {
 	size_t count = 0;
 
-	while (len > 0 && tolower(*str) == tolower(*prefix)) {
+	while (len > 0 && tolower((unsigned char)*str) == tolower((unsigned char)*prefix)) {
 		count++;
 		str++;
 		prefix++;
