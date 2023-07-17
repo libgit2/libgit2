62498558ffbd16449850d33c5048673fce78999e
Edward Thomson <ethomson@edwardthomson.com>
2023-07-17 11:05:48 +0100

    meta: update version numbers to v1.8

--- include/git2/version.h
+++ include/git2/version.h
@@ -11,13 +11,13 @@
  * The version string for libgit2.  This string follows semantic
  * versioning (v2) guidelines.
  */
-#define LIBGIT2_VERSION        "1.7.0"
+#define LIBGIT2_VERSION        "1.8.0-alpha"
 
 /** The major version number for this version of libgit2. */
 #define LIBGIT2_VER_MAJOR      1
 
 /** The minor version number for this version of libgit2. */
-#define LIBGIT2_VER_MINOR      7
+#define LIBGIT2_VER_MINOR      8
 
 /** The revision ("teeny") version number for this version of libgit2. */
 #define LIBGIT2_VER_REVISION   0
@@ -31,9 +31,13 @@
  * a prerelease name like "beta" or "rc1".  For final releases, this will
  * be `NULL`.
  */
-#define LIBGIT2_VER_PRERELEASE NULL
+#define LIBGIT2_VER_PRERELEASE "alpha"
 
-/** The library ABI soversion for this version of libgit2. */
+/**
+ * The library ABI soversion for this version of libgit2. This should
+ * only be changed when the library has a breaking ABI change, and so
+ * may trail the library's version number.
+ */
 #define LIBGIT2_SOVERSION      "1.7"
 
 #endif
