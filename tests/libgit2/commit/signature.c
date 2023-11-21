a8cca4d5373f94cb5838715e0d3381a0f1dd79fb
Yuriy Chernyshov <thegeorg@yandex-team.com>
2023-11-21 15:25:45 +0300

    Update tests

--- tests/libgit2/commit/signature.c
+++ tests/libgit2/commit/signature.c
@@ -39,7 +39,7 @@ void test_commit_signature__leading_and_trailing_spaces_are_trimmed(void)
 void test_commit_signature__leading_and_trailing_crud_is_trimmed(void)
 {
 	assert_name_and_email("nulltoken", "emeric.fermas@gmail.com", "\"nulltoken\"", "\"emeric.fermas@gmail.com\"");
-	assert_name_and_email("nulltoken w", "emeric.fermas@gmail.com", "nulltoken w.", "emeric.fermas@gmail.com");
+	assert_name_and_email("nulltoken w", "emeric.fermas@gmail.com", "nulltoken w;", "emeric.fermas@gmail.com");
 	assert_name_and_email("nulltoken \xe2\x98\xba", "emeric.fermas@gmail.com", "nulltoken \xe2\x98\xba", "emeric.fermas@gmail.com");
 }
 
