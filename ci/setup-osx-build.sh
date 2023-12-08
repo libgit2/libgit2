e002004686109945cbf6089e18bc65d1a85bce12
Edward Thomson <ethomson@edwardthomson.com>
2023-12-08 16:22:44 +0000

    ci: use Ninja on macOS

--- ci/setup-osx-build.sh
+++ ci/setup-osx-build.sh
@@ -3,6 +3,6 @@
 set -ex
 
 brew update
-brew install pkgconfig libssh2
+brew install pkgconfig libssh2 ninja
 
 ln -s /Applications/Xcode.app/Contents/Developer/usr/lib/libLeaksAtExit.dylib /usr/local/lib
