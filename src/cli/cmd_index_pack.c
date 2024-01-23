6fd6a7cf9b10771b8ad80e7dc2a3534e732c42c3
Edward Thomson <ethomson@edwardthomson.com>
2024-01-23 14:23:43 +0000

    cli: index-pack handles read errors gracefully

--- src/cli/cmd_index_pack.c
+++ src/cli/cmd_index_pack.c
@@ -95,6 +95,11 @@ int cmd_index_pack(int argc, char **argv)
 		}
 	}
 
+	if (read_len < 0) {
+		ret = cli_error_os();
+		goto done;
+	}
+
 	if (git_indexer_commit(idx, &stats) < 0) {
 		ret = cli_error_git();
 		goto done;
