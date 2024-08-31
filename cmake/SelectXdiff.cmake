25d06728cc5e1250a7d7ccf43f601966c875216e
flagarde <flagarde.programs@pm.me>
2024-09-01 01:46:13 +0800

    targets

--- cmake/SelectXdiff.cmake
+++ cmake/SelectXdiff.cmake
@@ -1,9 +1,9 @@
 # Optional external dependency: xdiff
+add_library(libgit2_xdiff INTERFACE)
 if(USE_XDIFF STREQUAL "system")
 	message(FATAL_ERROR "external/system xdiff is not yet supported")
 else()
 	add_subdirectory("${PROJECT_SOURCE_DIR}/deps/xdiff" "${PROJECT_BINARY_DIR}/deps/xdiff")
-	list(APPEND LIBGIT2_DEPENDENCY_INCLUDES "${PROJECT_SOURCE_DIR}/deps/xdiff")
-	list(APPEND LIBGIT2_DEPENDENCY_OBJECTS "$<TARGET_OBJECTS:xdiff>")
+	target_link_libraries(libgit2_xdiff INTERFACE xdiff)
 	add_feature_info(xdiff ON "xdiff support (bundled)")
 endif()
