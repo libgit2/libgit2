88bd589f7c01a9106c74f09815c8a0e5ca066b67
Hamed Masafi <hamed.masafi@gmail.com>
2024-09-14 12:03:17 +0330

    Add VER_CHECK macro

--- include/git2/version.h
+++ include/git2/version.h
@@ -40,4 +40,20 @@
  */
 #define LIBGIT2_SOVERSION      "1.8"
 
+/* 
+ * LIBGIT2_VERSION_CHECK:
+ * This macro can be used to compare against a specific libgit2 version.
+ * It takes the major, minor, and patch version as parameters.
+ * 
+ * Usage Example:
+ * 
+ *   #if LIBGIT2_VERSION_CHECK(1, 7, 0) >= LIBGIT2_VERSION_NUMBER
+ *     // This code will only compile if libgit2 version is >= 1.7.0
+ *   #endif
+ */
+#define LIBGIT2_VER_CHECK(major, minor, patch)    ((major<<16)|(minor<<8)|(patch))
+
+/* Macro to get the current version as a single integer */
+#define LIBGIT2_VER_NUMBER LIBGIT2_VERSION_CHECK(LIBGIT2_VER_MAJOR, LIBGIT2_VER_MINOR, LIBGIT2_VER_PATCH)
+
 #endif
