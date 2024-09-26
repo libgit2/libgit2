41f6f729104c770f59767d44efba60f1366ed571
Edward Thomson <ethomson@edwardthomson.com>
2024-09-26 16:59:53 +0200

    iOS Updates
    Some minor refactoring for iOS:
    
    - Roll back clar changes; these should be a bit more measured, and occur
      in clar upstream.
    - Move iOS to nightly builds

--- ci/setup-ios-benchmark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-brew update
-brew install hyperfine
--- ci/setup-ios-build.sh
+++ ci/setup-ios-build.sh
@@ -7,6 +7,4 @@ brew install pkgconfig libssh2 ninja
 
 ln -s /Applications/Xcode.app/Contents/Developer/usr/lib/libLeaksAtExit.dylib /usr/local/lib
 
-# The above is copied from setup-osx-build.sh
-
 curl -s -L https://raw.githubusercontent.com/leetal/ios-cmake/master/ios.toolchain.cmake -o ios.toolchain.cmake
