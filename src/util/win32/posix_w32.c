57ae4b11b6d883b67e6bfb0d996c89e227d55527
Laurence McGlashan <mail@laurencemcglashan.com>
2024-10-25 20:29:55 +0100

    util/win32: Continue if access is denied when deleting a folder.

--- src/util/win32/posix_w32.c
+++ src/util/win32/posix_w32.c
@@ -770,6 +770,7 @@ int p_rmdir(const char *path)
 			 * handle to the directory."  This sounds like what everybody else calls
 			 * EBUSY.  Let's convert appropriate error codes.
 			 */
+			case ERROR_ACCESS_DENIED:
 			case ERROR_SHARING_VIOLATION:
 				errno = EBUSY;
 				break;
