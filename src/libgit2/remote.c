83994dafec424346b9f5752fd7a44bd95b9598cd
Ryan Pham <phamcake@gmail.com>
2024-12-24 10:39:27 +0900

    remote: Remove unnecessary call
    `git_refspec_is_negative()` is already called by
    `git_refspec_src_matches_negative()`, so should not be necessary here.

--- src/libgit2/remote.c
+++ src/libgit2/remote.c
@@ -2635,7 +2635,7 @@ git_refspec *git_remote__matching_refspec(git_remote *remote, const char *refnam
 		if (spec->push)
 			continue;
 
-		if (git_refspec_is_negative(spec) && git_refspec_src_matches_negative(spec, refname))
+		if (git_refspec_src_matches_negative(spec, refname))
 			return NULL;
 
 		if (git_refspec_src_matches(spec, refname))
