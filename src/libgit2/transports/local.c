80fd2969ba8e1a444d5b477553b784701069ffb1
mwood <mwood@MATHWORKS-ZDMNG>
2025-06-20 16:46:57 +0100

    Combine some duplicated error handling.

--- src/libgit2/transports/local.c
+++ src/libgit2/transports/local.c
@@ -299,14 +299,8 @@ static int local_negotiate_fetch(
 
 	GIT_UNUSED(wants);
 
-	if (wants->depth) {
+	if (wants->depth || wants->shallow_since) {
 		git_error_set(GIT_ERROR_NET, "shallow fetch is not supported by the local transport");
-		return GIT_ENOTSUPPORTED;
-	}
-	if (wants->shallow_since) {
-		git_error_set(
-		        GIT_ERROR_NET,
-		        "shallow-since fetch is not supported by the local transport");
 		return GIT_ENOTSUPPORTED;
 	}
 
