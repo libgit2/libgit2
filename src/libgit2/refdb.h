b1ac78ecb9a14947eccaee8b6ea0cedab5046ce5
Patrick Steinhardt <ps@pks.im>
2025-07-11 12:07:26 +0200

    repository: allow initialization with a specific refdb type
    While we only support initializing repositories with the "files"
    reference backend right now, we are in the process of implementing a
    second backend with the "reftable" format. And while we already have the
    infrastructure to decide which format a repository should use when we
    open it, we do not have infrastructure yet to create new repositories
    with a different reference format.
    
    Introduce a new field `git_repository_init_options::refdb_type`. If
    unset, we'll default to the "files" backend. Otherwise though, if set to
    a valid `git_refdb_t`, we will use that new format to initialize the
    repostiory.
    
    Note that for now the only thing we do is to write the "refStorage"
    extension accordingly. What we explicitly don't yet do is to also handle
    the backend-specific logic to initialize the refdb on disk. This will be
    implemented in subsequent commits.

--- src/libgit2/refdb.h
+++ src/libgit2/refdb.h
@@ -125,6 +125,16 @@ int git_refdb_ensure_log(git_refdb *refdb, const char *refname);
 int git_refdb_lock(void **payload, git_refdb *db, const char *refname);
 int git_refdb_unlock(git_refdb *db, void *payload, int success, int update_reflog, const git_reference *ref, const git_signature *sig, const char *message);
 
+GIT_INLINE(const char *) git_refdb_type_name(git_refdb_t type)
+{
+	switch (type) {
+	case GIT_REFDB_FILES:
+		return "files";
+	default:
+		return "unknown";
+	}
+}
+
 GIT_INLINE(git_refdb_t) git_refdb_type_fromstr(const char *name)
 {
 	if (strcmp(name, "files") == 0)
