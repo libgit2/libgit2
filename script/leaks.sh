399292dd5f2e9f0e59320e56c0569841a5267264
Łukasz Langa <lukasz@langa.pl>
2025-12-11 18:11:50 +0100

    Some versions of `leaks` want -exclude to not have a space

--- script/leaks.sh
+++ script/leaks.sh
@@ -4,5 +4,5 @@ export MallocScribble=1
 export MallocLogFile=/dev/null
 # Exclude known Apple Security framework leak in CryptKit::FEEKeyInfoProvider
 # which occurs during SSL/TLS handshakes and is not in libgit2's control
-export CLAR_AT_EXIT="leaks -quiet -exclude SSLHandshake \$PPID"
+export CLAR_AT_EXIT="leaks -quiet -exclude=SSLHandshake \$PPID"
 exec "$@"
