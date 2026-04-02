9e3b00acfb46f09b9e9344ed8a3b358792ffd79b
Patrick Steinhardt <ps@pks.im>
2026-04-02 08:20:40 +0200

    deps/reftable: implement system compatibility code
    While the reftable library is mostly decoupled from the Git codebase,
    some specific functionality intentionally taps into Git subsystems. To
    make porting of the reftable library easier all of these are contained
    in "system.h" and "system.c".
    
    Reimplement those compatibility shims so that they work for libgit2.

--- deps/reftable/reftable-system.h
+++ deps/reftable/reftable-system.h
@@ -8,11 +8,26 @@
  * header is expected to be changed by the individual project.
  */
 
-#define MINGW_DONT_HANDLE_IN_USE_ERROR
-#include "compat/posix.h"
-#include "compat/zlib-compat.h"
+#include "map.h"
+#include "posix.h"
+#include "util.h"
+#include <zlib.h>
 
-int reftable_fsync(int fd);
-#define fsync(fd) reftable_fsync(fd)
+/*
+ * We only need to redefine on Windows as we expect stat(3p) et al to be
+ * available on Unix platforms. Furthermore, we only need to redefine fstat(3p)
+ * because we already redefine stat(3p) "win32-compat.h".
+ */
+#ifdef GIT_WIN32
+#  define fstat(fd, st) p_fstat(fd, st)
+#endif
+
+#define fsync(fd) p_fsync(fd)
+
+#define poll(fds, fds_len, timeout) p_poll(fds, fds_len, timeout)
+
+#define REFTABLE_ALLOW_BANNED_ALLOCATORS
+
+#define inline GIT_INLINE_KEYWORD
 
 #endif
