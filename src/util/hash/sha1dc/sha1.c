c0d9b86cfcf3cb348d3ffac09dab404a8006423f
Yvonne <yvonnelxxxx@gmail.com>
2026-08-18 15:24:49 +0800

    fix: misaligned-uint32_t load in sha1_compression_states

--- src/util/hash/sha1dc/sha1.c
+++ src/util/hash/sha1dc/sha1.c
@@ -1838,13 +1838,8 @@ void SHA1DCUpdate(SHA1_CTX *ctx, const char *buf, size_t len)
 	while (len >= 64)
 	{
 		ctx->total += 64;
-
-#if defined(SHA1DC_ALLOW_UNALIGNED_ACCESS)
-		sha1_process(ctx, (uint32_t*)(buf));
-#else
 		memcpy(ctx->buffer, buf, 64);
 		sha1_process(ctx, (uint32_t*)(ctx->buffer));
-#endif /* defined(SHA1DC_ALLOW_UNALIGNED_ACCESS) */
 		buf += 64;
 		len -= 64;
 	}
